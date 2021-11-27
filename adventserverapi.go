9cbbd5f550b4a4899d65b72d968c877215f4b7d5
Simon Tucker <brotherlogic@gmail.com>
2021-11-27 10:18:28 -0800

    Addresses nil error

--- adventserverapi.go
+++ adventserverapi.go
@@ -19,5 +19,8 @@ var (
 
 func (s *Server) Solve(ctx context.Context, req *pb.SolveRequest) (*pb.SolveResponse, error) {
 	val := reflect.ValueOf(s).MethodByName("Solve2016day1part2").Call([]reflect.Value{reflect.ValueOf(ctx)})
+	if val[1].Interface() == nil {
+		return val[0].Interface().(*pb.SolveResponse), nil
+	}
 	return val[0].Interface().(*pb.SolveResponse), val[1].Interface().(error)
 }
