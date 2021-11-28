e34307c198617463fd328dda34d4368c88591d45
Simon Tucker <brotherlogic@gmail.com>
2021-11-28 06:16:07 -0800

    Adds day2 part1

--- advertserverapi_test.go
+++ advertserverapi_test.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"context"
-	"log"
-	"reflect"
 	"testing"
 )
 
 func TestReflect(t *testing.T) {
-	s := Init()
+	//s := Init()
 
-	ctx := context.Background()
-	val := reflect.ValueOf(s).MethodByName("Solve2016day1part2").Call([]reflect.Value{reflect.ValueOf(ctx)})
-	log.Printf("%v -> %v", ctx, val)
+	//ctx := context.Background()
+	//val := reflect.ValueOf(s).MethodByName("Solve2016day1part2").Call([]reflect.Value{reflect.ValueOf(ctx)})
+	//log.Printf("%v -> %v", ctx, val)
 }
