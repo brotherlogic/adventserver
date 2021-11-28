cffb8310bc565bc4e9f7a25a08a838e5cdaa3694
Simon Tucker <brotherlogic@gmail.com>
2021-11-28 08:02:27 -0800

    Worked on part 2

--- 2017-2.go
+++ 2017-2.go
@@ -36,6 +36,31 @@ func getChecksum(str string) int32 {
 	return int32(checksum)
 }
 
+func evenDivide(str string) int32 {
+	checksum := 0
+
+	for _, line := range strings.Split(str, "\n") {
+		for i, digit := range strings.Fields(line) {
+			for j := i + 1; j < len(strings.Fields(line)); j++ {
+				onum, err2 := strconv.Atoi(strings.Fields(line)[j])
+				num, err := strconv.Atoi(digit)
+				if err != nil || err2 != nil {
+					log.Fatalf("Bad number: %v or %v", digit, strings.Fields(line)[j])
+				}
+
+				if num%onum == 0 {
+					checksum += num / onum
+				}
+				if onum%num == 0 {
+					checksum += onum / num
+				}
+			}
+		}
+	}
+
+	return int32(checksum)
+}
+
 func (s *Server) Solve2017day2part1(ctx context.Context) (*pb.SolveResponse, error) {
 	data, err := s.loadFile(ctx, "/media/scratch/advent/2017-2.txt")
 	if err != nil {
@@ -44,4 +69,14 @@ func (s *Server) Solve2017day2part1(ctx context.Context) (*pb.SolveResponse, err
 	trimmed := strings.TrimSpace(data)
 
 	return &pb.SolveResponse{Answer: getChecksum(trimmed)}, nil
+}
+
+func (s *Server) Solve2017day2part2(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2017-2.txt")
+	if err != nil {
+		return nil, err
+	}
+	trimmed := strings.TrimSpace(data)
+
+	return &pb.SolveResponse{Answer: evenDivide(trimmed)}, nil
 }
--- 2017-2_test.go
+++ 2017-2_test.go
@@ -11,3 +11,13 @@ func Test2017Day2Part1(t *testing.T) {
 		t.Errorf("Bad checksum: %v vs %v", res, 18)
 	}
 }
+
+func Test2017Day2Part2(t *testing.T) {
+	input := "5 9 2 8\n9 4 7 3\n3 8 6 5"
+
+	res := evenDivide(input)
+
+	if res != int32(9) {
+		t.Errorf("Bad checksum: %v vs %v", res, 9)
+	}
+}
