763aa58e5e0e9b25d04d1ecacc184b4d21460182
Simon Tucker <brotherlogic@gmail.com>
2021-11-30 21:10:58 -0800

    Adds in

--- 2017-3.go
+++ 2017-3.go
@@ -31,8 +31,8 @@ func computeSpiral(n int) int {
 
 func computeSpiralPoint(values [][]int, x, y int) int {
 	sumv := 0
-	for i := -1; i <= 1; i++ {
-		for j := 1; j <= 1; j++ {
+	for i := x - 1; i <= x+1; i++ {
+		for j := y - 1; j <= y+1; j++ {
 			if i < len(values) && i >= 0 && j < len(values[i]) && j >= 0 {
 				sumv += values[i][j]
 			}
@@ -43,18 +43,55 @@ func computeSpiralPoint(values [][]int, x, y int) int {
 
 func buildSpiral(n int) int {
 	var values [][]int
-	for i := 0; i < 100; i++ {
-		values = append(values, make([]int, 100))
+	width := 50
+	for i := 0; i < width*2; i++ {
+		values = append(values, make([]int, width*2))
 	}
 
-	spx := 50
-	spy := 50
+	spx := width
+	spy := width
 
 	values[spx][spy] = 1
+	for w := 1; w < width; w++ {
+		nspx := width + w
+		nspy := width - w + 1
+
+		dir := 0
+		for count := 0; count < w*8; count++ {
+			value := computeSpiralPoint(values, nspx, nspy)
+			if value > n {
+				return value
+			}
+			values[nspx][nspy] = value
+			switch dir {
+			case 0:
+				nspy++
+				if nspy-width == w {
+					dir = 1
+				}
+			case 1:
+				nspx--
+				if width-nspx == w {
+					dir = 2
+				}
+			case 2:
+				nspy--
+				if width-nspy == w {
+					dir = 3
+				}
+			case 3:
+				nspx++
+			}
+		}
+	}
 
 	return 0
 }
 
 func (s *Server) Solve2017day3part1(ctx context.Context) (*pb.SolveResponse, error) {
 	return &pb.SolveResponse{Answer: int32(computeSpiral(265149))}, nil
+}
+
+func (s *Server) Solve2017day3part2(ctx context.Context) (*pb.SolveResponse, error) {
+	return &pb.SolveResponse{Answer: int32(buildSpiral(265149))}, nil
 }
--- 2017-3_test.go
+++ 2017-3_test.go
@@ -20,3 +20,19 @@ func Test2017Day3P1(t *testing.T) {
 		}
 	}
 }
+
+func Test2017Day3P2(t *testing.T) {
+	cases := []struct {
+		in   int
+		want int
+	}{
+		{120, 122},
+	}
+
+	for _, c := range cases {
+		got := buildSpiral(c.in)
+		if int(got) != c.want {
+			t.Errorf("Spec(%v) == %d, want %d", c.in, got, c.want)
+		}
+	}
+}
