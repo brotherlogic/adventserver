b766aa5dd7ad6ee7a88738c26a7abf51c2e24fab
Simon Tucker <brotherlogic@gmail.com>
2021-12-01 21:19:10 -0800

    Adds day2 part 2

--- 2021-2.go
+++ 2021-2.go
@@ -28,6 +28,28 @@ func procSub(str string) int {
 	return pos * depth
 }
 
+func procSubAdv(str string) int {
+	pos := 0
+	depth := 0
+	aim := 0
+
+	for _, st := range strings.Split(str, "\n") {
+		elems := strings.Fields(st)
+		dist, _ := strconv.Atoi(elems[1])
+		switch elems[0] {
+		case "forward":
+			pos += dist
+			depth += aim * dist
+		case "down":
+			aim += dist
+		case "up":
+			aim -= dist
+		}
+
+	}
+	return pos * depth
+}
+
 func (s *Server) Solve2021day2part1(ctx context.Context) (*pb.SolveResponse, error) {
 	data, err := s.loadFile(ctx, "/media/scratch/advent/2021-2.txt")
 	if err != nil {
@@ -36,4 +58,14 @@ func (s *Server) Solve2021day2part1(ctx context.Context) (*pb.SolveResponse, err
 	trimmed := strings.TrimSpace(data)
 
 	return &pb.SolveResponse{Answer: int32(procSub(trimmed))}, nil
+}
+
+func (s *Server) Solve2021day2part2(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2021-2.txt")
+	if err != nil {
+		return nil, err
+	}
+	trimmed := strings.TrimSpace(data)
+
+	return &pb.SolveResponse{Answer: int32(procSubAdv(trimmed))}, nil
 }
