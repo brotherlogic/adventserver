59bc4ca22fcc631cef0dc54579f4d271fb289c07
Simon Tucker <brotherlogic@gmail.com>
2021-12-02 17:36:08 -0800

    Covers part 2

--- 2017-4.go
+++ 2017-4.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"sort"
 	"strings"
 
 	pb "github.com/brotherlogic/adventserver/proto"
 	"golang.org/x/net/context"
 )
 
-func isValidPassword(in string) bool {
+func sortWord(str string) string {
+	var intarr []int
+	for _, c := range str {
+		intarr = append(intarr, int(c))
+	}
+
+	sort.Ints(intarr)
+
+	strret := ""
+	for _, elem := range intarr {
+		strret += string(rune(elem))
+	}
+	return strret
+}
+
+func isValidPassword(in string, srt bool) bool {
 	counts := make(map[string]int)
 	for _, word := range strings.Fields(in) {
+		if srt {
+			word = sortWord(word)
+		}
 		counts[word]++
 	}
 
@@ -31,7 +50,24 @@ func (s *Server) Solve2017day4part1(ctx context.Context) (*pb.SolveResponse, err
 
 	count := 0
 	for _, str := range strings.Split(trimmed, "\n") {
-		if isValidPassword(str) {
+		if isValidPassword(str, false) {
+			count++
+		}
+	}
+
+	return &pb.SolveResponse{Answer: int32(count)}, nil
+}
+
+func (s *Server) Solve2017day4part2(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2017-d4.txt")
+	if err != nil {
+		return nil, err
+	}
+	trimmed := strings.TrimSpace(data)
+
+	count := 0
+	for _, str := range strings.Split(trimmed, "\n") {
+		if isValidPassword(str, true) {
 			count++
 		}
 	}
--- 2017-4_test.go
+++ 2017-4_test.go
@@ -13,7 +13,27 @@ func TestPasphrase(t *testing.T) {
 	}
 
 	for _, c := range cases {
-		got := isValidPassword(c.in)
+		got := isValidPassword(c.in, false)
+		if got != c.want {
+			t.Errorf("Spec(%v) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
+
+func TestPasphrase2(t *testing.T) {
+	cases := []struct {
+		in   string
+		want bool
+	}{
+		{"abcde fghij", true},
+		{"abcde xyz ecdab", false},
+		{"a ab abc abd abf abj", true},
+		{"iiii oiii ooii oooi oooo", true},
+		{"oiii ioii iioi iiio", false},
+	}
+
+	for _, c := range cases {
+		got := isValidPassword(c.in, true)
 		if got != c.want {
 			t.Errorf("Spec(%v) == %v, want %v", c.in, got, c.want)
 		}
