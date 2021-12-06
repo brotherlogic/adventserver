32b4f714dd6ff8d62316cdd6ef2209a87af8b059
Simon Tucker <brotherlogic@gmail.com>
2021-12-06 07:20:12 -0800

    Convert to int64s

--- 2021-7.go
+++ 2021-7.go
@@ -8,8 +8,8 @@ import (
 	"golang.org/x/net/context"
 )
 
-func runCycle(mapper map[int]int) map[int]int {
-	nmapper := make(map[int]int)
+func runCycle(mapper map[int]int64) map[int]int64 {
+	nmapper := make(map[int]int64)
 	for key, count := range mapper {
 		if key == 0 {
 			nmapper[6] += count
@@ -22,8 +22,8 @@ func runCycle(mapper map[int]int) map[int]int {
 	return nmapper
 }
 
-func computeCycle(list string, days int) int {
-	mapper := make(map[int]int)
+func computeCycle(list string, days int) int64 {
+	mapper := make(map[int]int64)
 
 	for _, elem := range strings.Split(list, ",") {
 		val, _ := strconv.Atoi(elem)
@@ -34,7 +34,7 @@ func computeCycle(list string, days int) int {
 		mapper = runCycle(mapper)
 	}
 
-	count := 0
+	count := int64(0)
 	for _, c := range mapper {
 		count += c
 	}
@@ -56,5 +56,5 @@ func (s *Server) Solve2021day7part2(ctx context.Context) (*pb.SolveResponse, err
 		return nil, err
 	}
 
-	return &pb.SolveResponse{Answer: int32(computeCycle(strings.TrimSpace(data), 256))}, nil
+	return &pb.SolveResponse{BigAnswer: computeCycle(strings.TrimSpace(data), 256)}, nil
 }
