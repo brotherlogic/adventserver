6790612faf29d1047619aa26264ec4803f5e5a84
Simon Tucker <brotherlogic@gmail.com>
2021-12-05 17:47:07 -0800

    Also does part 2

--- 2017-6.go
+++ 2017-6.go
@@ -27,23 +27,29 @@ func doNext(blocks []int) []int {
 	return blocks
 }
 
-func computeRepeat(blocks []int) int {
-	seen := make(map[string]bool)
+func computeRepeat(blocks []int) (int, int) {
+	seen := make(map[string]int)
 	count := 0
-	seen[fmt.Sprintf("%v", blocks)] = true
+	seen[fmt.Sprintf("%v", blocks)] = 1
 
 	for {
 		blocks = doNext(blocks)
 
-		if seen[fmt.Sprintf("%v", blocks)] {
-			return count + 1
+		if seen[fmt.Sprintf("%v", blocks)] > 0 {
+			return count + 1, count - seen[fmt.Sprintf("%v", blocks)] + 1
 		} else {
 			count++
-			seen[fmt.Sprintf("%v", blocks)] = true
+			seen[fmt.Sprintf("%v", blocks)] = count
 		}
 	}
 }
 
 func (s *Server) Solve2017day6part1(ctx context.Context) (*pb.SolveResponse, error) {
-	return &pb.SolveResponse{Answer: int32(computeRepeat([]int{4, 10, 4, 1, 8, 4, 9, 14, 5, 1, 14, 15, 0, 15, 3, 5}))}, nil
+	a1, _ := computeRepeat([]int{4, 10, 4, 1, 8, 4, 9, 14, 5, 1, 14, 15, 0, 15, 3, 5})
+	return &pb.SolveResponse{Answer: int32(a1)}, nil
+}
+
+func (s *Server) Solve2017day6part2(ctx context.Context) (*pb.SolveResponse, error) {
+	_, a1 := computeRepeat([]int{4, 10, 4, 1, 8, 4, 9, 14, 5, 1, 14, 15, 0, 15, 3, 5})
+	return &pb.SolveResponse{Answer: int32(a1)}, nil
 }
--- 2017-6_test.go
+++ 2017-6_test.go
@@ -5,9 +5,13 @@ import "testing"
 func Test2017Day6Part1(t *testing.T) {
 	blocks := []int{0, 2, 7, 0}
 
-	val := computeRepeat(blocks)
+	val, val2 := computeRepeat(blocks)
 
 	if val != 5 {
 		t.Errorf("Bad repeat: %v vs 5", val)
+	}
+
+	if val2 != 4 {
+		t.Errorf("Bad length: %v vs 5", val2)
 	}
 }
