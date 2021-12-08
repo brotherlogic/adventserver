6cd3c90df4a3f7349652bc2fad5168bfa27b844f
Simon Tucker <brotherlogic@gmail.com>
2021-12-07 17:59:16 -0800

    2021 day 7

---
+++ 2021-7.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"math"
+	"strconv"
+	"strings"
+
+	pb "github.com/brotherlogic/adventserver/proto"
+	"golang.org/x/net/context"
+)
+
+func computeMin(nums []int) int {
+	mx := nums[0]
+	mi := nums[0]
+	for _, num := range nums {
+		if num > mx {
+			mx = num
+		}
+
+		if num < mi {
+			mi = num
+		}
+	}
+
+	best := math.MaxInt32
+	for i := mi; i <= mx; i++ {
+		sumv := 0
+		for _, num := range nums {
+			sumv += abs(num - i)
+		}
+
+		if sumv < best {
+			best = sumv
+		}
+	}
+
+	return best
+}
+
+func getCost(str string) int {
+	var nums []int
+
+	for _, elem := range strings.Split(str, ",") {
+		n, _ := strconv.Atoi(strings.TrimSpace(elem))
+		nums = append(nums, n)
+	}
+
+	return computeMin(nums)
+}
+
+func (s *Server) Solve2021day7part1(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2021-7.txt")
+	if err != nil {
+		return nil, err
+	}
+	trimmed := strings.TrimSpace(data)
+
+	return &pb.SolveResponse{Answer: int32(getCost(trimmed))}, nil
+}
---
+++ 2021-7_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func Test2021Day7(t *testing.T) {
+	start := "16,1,2,0,4,2,7,1,2,14"
+
+	cost := getCost(start)
+	if cost != 37 {
+		t.Errorf("Bad cost: %v vs 37", cost)
+	}
+}
