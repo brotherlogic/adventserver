9575b3f25d246fe78bf314b023ebc03159c28642
Simon Tucker <brotherlogic@gmail.com>
2021-12-11 09:12:43 -0800

    Adds 2021 day 11

---
+++ 2021-11.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	pb "github.com/brotherlogic/adventserver/proto"
+	"golang.org/x/net/context"
+)
+
+type octo struct {
+	num     int
+	flashed bool
+}
+
+func buildArr(data string) [][]*octo {
+	var octoArr [][]*octo
+
+	for _, line := range strings.Split(strings.TrimSpace(data), "\n") {
+		var octoIn []*octo
+		for _, c := range strings.TrimSpace(line) {
+			num, _ := strconv.Atoi(string(c))
+			octoIn = append(octoIn, &octo{num: num})
+		}
+
+		octoArr = append(octoArr, octoIn)
+	}
+	return octoArr
+}
+
+func restoreString(octoArr [][]*octo) string {
+	data := ""
+	for _, line := range octoArr {
+		for _, c := range line {
+			data += fmt.Sprintf("%v", c.num)
+		}
+		data += "\n"
+	}
+
+	return strings.TrimSpace(data)
+}
+
+func runFlash(octoArr [][]*octo) int {
+	flashCount := 0
+
+	for i, octoLine := range octoArr {
+		for j, oc := range octoLine {
+			if oc.num > 9 && !oc.flashed {
+				oc.num = 0
+				oc.flashed = true
+				flashCount++
+
+				//Run the increment
+				for x := max(0, i-1); x < min(i+2, len(octoArr)); x++ {
+					for y := max(0, j-1); y < min(j+2, len(octoLine)); y++ {
+						if x != i || y != j {
+							if !octoArr[x][y].flashed {
+								octoArr[x][y].num++
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return flashCount
+}
+
+func flash(octoArr [][]*octo) int {
+	//Clear all flashes
+	for _, line := range octoArr {
+		for _, oc := range line {
+			oc.flashed = false
+		}
+	}
+
+	//First increment everything
+	for _, octoLine := range octoArr {
+		for _, oc := range octoLine {
+			oc.num++
+		}
+	}
+
+	//Now flash
+	fullCount := 0
+	fc := runFlash(octoArr)
+	for fc != 0 {
+		fullCount += fc
+		fc = runFlash(octoArr)
+	}
+
+	return fullCount
+}
+
+func (s *Server) Solve2021day10part2(ctx context.Context) (*pb.SolveResponse, error) {
+	data := `2566885432
+	3857414357
+	6761543247
+	5477332114
+	3731585385
+	1716783173
+	1277321612
+	3371176148
+	1162578285
+	6144726367`
+
+	arr := buildArr(data)
+	count := int32(0)
+	for i := 0; i < 100; i++ {
+		count += int32(flash(arr))
+	}
+
+	return &pb.SolveResponse{Answer: count}, nil
+}
---
+++ 2021-11_test.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"testing"
+)
+
+func Test2021Day11(t *testing.T) {
+	data := `11111
+	19991
+	19191
+	19991
+	11111`
+
+	arr := buildArr(data)
+	count := flash(arr)
+	if count != 9 {
+		t.Errorf("Did not flash the riight number: %v and \n%v", count, restoreString(arr))
+	}
+}
+
+func Test2021Specific(t *testing.T) {
+
+	data2 := `8807476555
+	5089087054
+	8597889608
+	8485769600
+	8700908800
+	6600088989
+	6800005943
+	0000007456
+	9000000876
+	8700006848`
+
+	arr2 := buildArr(data2)
+	flash(arr2)
+	log.Printf("\n%v", restoreString(arr2))
+}
+
+func Test2021Day11Part2(t *testing.T) {
+	data := `5483143223
+	2745854711
+	5264556173
+	6141336146
+	6357385478
+	4167524645
+	2176841721
+	6882881134
+	4846848554
+	5283751526`
+
+	arr := buildArr(data)
+	count := 0
+	for i := 0; i < 10; i++ {
+		count += flash(arr)
+	}
+
+	if count != 204 {
+		t.Errorf("Bad flash count: %v vs 204", count)
+	}
+
+	for i := 0; i < 90; i++ {
+		count += flash(arr)
+	}
+
+	if count != 1656 {
+		t.Errorf("Bad flash count: %v vs 1656", count)
+	}
+}
