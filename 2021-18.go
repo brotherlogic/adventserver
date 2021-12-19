58f1fa7ef611bc6ea33eb4ec6b2fe29ff49d15a4
Simon Tucker <brotherlogic@gmail.com>
2021-12-19 12:34:48 -0800

    Adds in day 18

---
+++ 2021-18.go
@@ -0,0 +1,270 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	pb "github.com/brotherlogic/adventserver/proto"
+	"golang.org/x/net/context"
+)
+
+type snnum struct {
+	val    int
+	left   *snnum
+	right  *snnum
+	parent *snnum
+}
+
+func explode(num *snnum) *snnum {
+	runExplode(num, nil, 0)
+
+	return num
+}
+
+func fullExplode(num *snnum) *snnum {
+	for runExplode(num, nil, 0) {
+		//Pass
+	}
+
+	return num
+}
+
+func reduceNum(num *snnum) *snnum {
+	for {
+		fullExplode(num)
+		if runSplit(num) {
+			split(num)
+		} else {
+			break
+		}
+	}
+
+	return num
+}
+
+func add(num1, num2 *snnum) *snnum {
+	val := &snnum{left: num1, right: num2, val: -1}
+	num1.parent = val
+	num2.parent = val
+	return val
+}
+
+func runSum(data string) *snnum {
+	elems := strings.Split(strings.TrimSpace(data), "\n")
+	num := parseNum(strings.TrimSpace(elems[0]))
+	for i := 1; i < len(elems); i++ {
+		nnum := reduceNum(add(num, parseNum(strings.TrimSpace(elems[i]))))
+		num = nnum
+	}
+
+	return num
+}
+
+func getLeft(num *snnum) *snnum {
+	root := num
+	for root.parent != nil {
+		root = root.parent
+	}
+
+	list := listify(root)
+	found := false
+	for i := len(list) - 1; i >= 0; i-- {
+		if list[i] == num {
+			found = true
+		} else if found && (list[i].val >= 0) {
+			return list[i]
+		}
+	}
+
+	return nil
+}
+
+func runSplit(num *snnum) bool {
+	if num == nil {
+		return false
+	}
+
+	if num.val > 9 {
+		split(num)
+		return true
+	}
+
+	return runSplit(num.left) || runSplit(num.right)
+}
+
+func split(num *snnum) *snnum {
+	if num.val > 9 {
+		adjust := 0
+		if num.val%2 != 0 {
+			adjust++
+		}
+		num.left = &snnum{parent: num, val: num.val / 2}
+		num.right = &snnum{parent: num, val: num.val/2 + adjust}
+		num.val = -1
+	}
+	return num
+}
+
+func getRight(num *snnum) *snnum {
+	root := num
+	for root.parent != nil {
+		root = root.parent
+	}
+
+	list := listify(root)
+	found := false
+	for i := 0; i < len(list); i++ {
+		if list[i] == num {
+			found = true
+		} else if found && (list[i].val >= 0) {
+			return list[i]
+		}
+	}
+
+	return nil
+}
+
+func runExplode(num *snnum, parent *snnum, depth int) bool {
+	if num == nil {
+		return false
+	}
+
+	if depth >= 4 && num.left != nil && num.right != nil && num.left.val >= 0 && num.right.val >= 0 {
+		// Run the explode
+		lnum := getLeft(num.left)
+		rnum := getRight(num.right)
+
+		if lnum != nil {
+			lnum.val += num.left.val
+		}
+		if rnum != nil {
+			rnum.val += num.right.val
+		}
+
+		num.left = nil
+		num.right = nil
+		num.val = 0
+
+		return true
+	}
+
+	// Search the left tree
+	found := runExplode(num.left, num, depth+1)
+
+	if !found {
+		return runExplode(num.right, num, depth+1)
+	}
+
+	return true
+}
+
+func listify(num *snnum) []*snnum {
+	var str []*snnum
+	if num.left != nil {
+		str = append(str, listify(num.left)...)
+	}
+
+	str = append(str, num)
+
+	if num.right != nil {
+		str = append(str, listify(num.right)...)
+	}
+
+	return str
+}
+
+func printNum(num *snnum) string {
+	if num == nil {
+		return "nil"
+	}
+	if num.val >= 0 {
+		return fmt.Sprintf("%v", num.val)
+	}
+	str := "["
+	if num.left != nil {
+		str += printNum(num.left)
+	}
+	str += ","
+
+	if num.right != nil {
+		str += printNum(num.right)
+	}
+
+	str += "]"
+	return str
+}
+
+func findLeftBalance(str string) int {
+	count := 0
+	for i := range str {
+		if string(str[i]) == "[" {
+			count++
+		} else if string(str[i]) == "]" {
+			count--
+		}
+
+		if count == 0 {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func findRightBalance(str string) int {
+	count := 0
+	for i := len(str) - 1; i >= 0; i-- {
+		if string(str[i]) == "]" {
+			count++
+		} else if string(str[i]) == "[" {
+			count--
+		}
+
+		if count == 0 {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func magnitude(num *snnum) int {
+	if num.val >= 0 {
+		return num.val
+	}
+	return 3*magnitude(num.left) + 2*magnitude(num.right)
+}
+
+func parseNum(str string) *snnum {
+	val, _ := parseNumInt(str, nil)
+	return val
+}
+
+func parseNumInt(str string, curr *snnum) (*snnum, string) {
+	ret := &snnum{parent: curr, val: -1}
+
+	switch string(str[0]) {
+	case "[":
+		val, rem := parseNumInt(str[1:], &snnum{parent: ret, val: -1})
+		ret.left = val
+		ret.right, rem = parseNumInt(rem[1:], &snnum{parent: ret, val: -1})
+		return ret, rem[1:]
+	case "]":
+		return ret, str[1:]
+	default:
+		val, _ := strconv.Atoi(string(str[0]))
+		ret.val = val
+		return ret, str[1:]
+	}
+}
+
+func (s *Server) Solve2021day18part1(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2021-18.txt")
+	if err != nil {
+		return nil, err
+	}
+	trimmed := strings.TrimSpace(data)
+
+	return &pb.SolveResponse{Answer: int32(magnitude(runSum(trimmed)))}, nil
+}
---
+++ 2021-18_test.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"log"
+	"testing"
+)
+
+func Test2021Day18Parse(t *testing.T) {
+	cases := []struct {
+		num string
+	}{
+		{"[1,2]"},
+		{"[[1,2],3]"},
+		{"[9,[8,7]]"},
+		{"[[1,9],[8,5]]"},
+		{"[[[[1,2],[3,4]],[[5,6],[7,8]]],9]"},
+		{"[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]"},
+		{"[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"},
+	}
+
+	for _, tc := range cases {
+		log.Printf("START_PARSE: %v", tc.num)
+		if tc.num != printNum(parseNum(tc.num)) {
+			t.Fatalf("Cannot process: %v -> %v", tc.num, printNum(parseNum(tc.num)))
+		}
+	}
+}
+
+func Test2021Day18Focus(t *testing.T) {
+	rep := parseNum("[[1,9],[8,5]]")
+	log.Printf("%v", rep)
+}
+
+func Test2021Day18Split(t *testing.T) {
+	cases := []struct {
+		start int
+		end   string
+	}{
+		{10, "[5,5]"},
+		{11, "[5,6]"},
+		{12, "[6,6]"},
+		{9, "9"},
+	}
+	for _, tc := range cases {
+		log.Printf("Splitting %v", tc.start)
+		val := printNum(split(&snnum{val: tc.start}))
+		if tc.end != val {
+			t.Fatalf("Cannot process: %v -> %v vs %v", tc.start, val, tc.end)
+		}
+	}
+}
+
+func Test2021Day18Explode(t *testing.T) {
+	cases := []struct {
+		start string
+		end   string
+	}{
+		{"[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]"},
+		{"[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]"},
+		{"[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]"},
+		{"[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"},
+		{"[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"},
+		{"[[[[0,9],2],3],4]", "[[[[0,9],2],3],4]"},
+	}
+
+	for _, tc := range cases {
+		log.Printf("Exploding %v", tc.start)
+		val := printNum(explode(parseNum(tc.start)))
+		if tc.end != val {
+			t.Fatalf("Cannot process: %v -> %v vs %v", tc.start, val, tc.end)
+		}
+	}
+}
+
+func Test2021Day18Part1(t *testing.T) {
+	data := `[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+	[[[5,[2,8]],4],[5,[[9,9],0]]]
+	[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+	[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+	[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+	[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+	[[[[5,4],[7,7]],8],[[8,3],8]]
+	[[9,3],[[9,9],[6,[4,9]]]]
+	[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+	[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]`
+
+	magnitude := magnitude(runSum(data))
+	if magnitude != 4140 {
+		t.Errorf("Bad mag: %v vs 4140", magnitude)
+	}
+}
+
+func Test2021Day18Magnitude(t *testing.T) {
+	cases := []struct {
+		start string
+		end   int
+	}{
+		{"[[1,2],[[3,4],5]]", 143},
+		{"[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384},
+		{"[[[[1,1],[2,2]],[3,3]],[4,4]]", 445},
+		{"[[[[3,0],[5,3]],[4,4]],[5,5]]", 791},
+		{"[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137},
+		{"[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488},
+	}
+
+	for _, tc := range cases {
+		log.Printf("Magnituding %v", tc.start)
+		val := magnitude(parseNum(tc.start))
+		if tc.end != val {
+			t.Fatalf("Cannot process: %v -> %v vs %v", tc.start, val, tc.end)
+		}
+	}
+}
+
+func Test2021Day18Reduce(t *testing.T) {
+	nnum := reduceNum(parseNum("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"))
+	if printNum(nnum) != "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]" {
+		t.Errorf("Unable to reduce -> %v", printNum(nnum))
+	}
+}
+
+func Test2021Day18Add(t *testing.T) {
+	nnum := add(parseNum("[[[[4,3],4],4],[7,[[8,4],9]]]"), parseNum("[1,1]"))
+	if printNum(nnum) != "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]" {
+		t.Errorf("Unable to add -> %v", printNum(nnum))
+	}
+}
+
+func Test2021Day18RunAddition(t *testing.T) {
+	cases := []struct {
+		start string
+		end   string
+	}{
+		{`[1,1]
+		[2,2]
+		[3,3]
+		[4,4]`, "[[[[1,1],[2,2]],[3,3]],[4,4]]"},
+		{`[1,1]
+		[2,2]
+		[3,3]
+		[4,4]
+		[5,5]`, "[[[[3,0],[5,3]],[4,4]],[5,5]]"},
+		{`[1,1]
+		[2,2]
+		[3,3]
+		[4,4]
+		[5,5]
+		[6,6]`, "[[[[5,0],[7,4]],[5,5]],[6,6]]"},
+		{`[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+		[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+		[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+		[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+		[7,[5,[[3,8],[1,4]]]]
+		[[2,[2,2]],[8,[8,1]]]
+		[2,9]
+		[1,[[[9,3],9],[[9,0],[0,7]]]]
+		[[[5,[7,4]],7],1]
+		[[[[4,2],2],6],[8,7]]`, "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"},
+	}
+
+	for _, tc := range cases {
+		val := runSum(tc.start)
+		if tc.end != printNum(val) {
+			t.Fatalf("Cannot process: %v -> %v vs %v", tc.start, printNum(val), tc.end)
+		}
+	}
+}
+
+func Test2021Day18SingleExplode(t *testing.T) {
+	num := parseNum("[[[[[1,1],[2,2]],[3,3]],[4,4]],[5,5]]")
+	log.Printf("WHAT %v", printNum(num))
+	res := runExplode(num, nil, 0)
+	if !res {
+		t.Errorf("Bad explode: %v", printNum(num))
+	}
+
+	log.Printf("AHA: %v", printNum(num))
+
+	v2 := runExplode(num, nil, 0)
+	log.Printf("NOW: %v, %v", v2, printNum(num))
+
+	v3 := runExplode(num, nil, 0)
+	log.Printf("NOW: %v, %v", v3, printNum(num))
+}
