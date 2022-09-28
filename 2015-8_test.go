2d61c09d157711fe4d232ba4ddb8e6697fe99d28
Simon Tucker <brotherlogic@gmail.com>
2022-09-28 08:42:13 -0700

    Fixed for updates

--- 2015-8_test.go
+++ 2015-8_test.go
@@ -9,7 +9,7 @@ func TestDay20158P1(t *testing.T) {
 		codes      int
 	}{
 		{`""`, 2, 0},
-		{`"abc"`, 5, 0},
+		{`"abc"`, 5, 3},
 		{`"aaa\"aaa"`, 10, 7},
 		{`"\x27"`, 6, 1},
 	}
@@ -17,10 +17,10 @@ func TestDay20158P1(t *testing.T) {
 	for _, c := range cases {
 		chs, cds := computeStringLength(c.in)
 		if chs != c.characters {
-			t.Fatalf("Wrong number of characters (%v should have been %v)", chs, c.characters)
+			t.Fatalf("Wrong number of characters on %v(%v should have been %v)", c.in, chs, c.characters)
 		}
 		if cds != c.codes {
-			t.Fatalf("Wrong string length (%v should have been %v)", cds, c.codes)
+			t.Fatalf("Wrong string length on %v (%v should have been %v)", c.in, cds, c.codes)
 		}
 	}
 }
