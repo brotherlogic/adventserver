babd43bbdd95d0fbb05ca244a71f482cd6d16fb5
Simon Tucker <brotherlogic@gmail.com>
2022-09-28 17:46:53 -0700

    Fix 2018 d8 p1 (#41)

--- 2015-8.go
+++ 2015-8.go
@@ -24,6 +24,20 @@ func computeStringLength(str string) (int, int) {
 	return len(str), count - 2
 }
 
+func computeStringAddition(str string) (int, int) {
+	var nstr strings.Builder
+	for i := 0; i < len(str); i++ {
+		if str[i] == '"' {
+			nstr.WriteString(`\"`)
+		} else if str[i] == '\\' {
+			nstr.WriteString(`\\`)
+		} else {
+			nstr.WriteString(string(str[i]))
+		}
+	}
+	return len(str), len(nstr.String()) + 2
+}
+
 func (s *Server) Solve2015day8part1(ctx context.Context) (*pb.SolveResponse, error) {
 	data, err := s.loadFile(ctx, "/media/scratch/advent/2015-8.txt")
 	if err != nil {
@@ -41,4 +55,23 @@ func (s *Server) Solve2015day8part1(ctx context.Context) (*pb.SolveResponse, err
 	}
 
 	return &pb.SolveResponse{Answer: int32(chs - cds)}, nil
+}
+
+func (s *Server) Solve2015day8part2(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2015-8.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	trimmed := strings.TrimSpace(data)
+
+	chs := 0
+	cds := 0
+	for _, str := range strings.Split(trimmed, "\n") {
+		c1, c2 := computeStringAddition(str)
+		chs += c1
+		cds += c2
+	}
+
+	return &pb.SolveResponse{Answer: int32(cds - chs)}, nil
 }
