c8f5ed2a421a8ba77ae73cd4b2e966f23a389051
Simon Tucker <brotherlogic@gmail.com>
2022-09-28 17:36:19 -0700

    Fixed

--- 2015-8.go
+++ 2015-8.go
@@ -21,7 +21,21 @@ func computeStringLength(str string) (int, int) {
 		count++
 	}
 
-	return len(str), count - 2
+	return len(str), len(str) + 4
+}
+
+func computeStringAddition(str string) (int, int) {
+	var nstr strings.Builder
+	for i := 0; i < len(str); i++ {
+		if str[i] == '"' {
+			nstr.WriteString(`\"`)
+		} else if str[i] == '\\' {
+			nstr.WriteString(`\\`)
+		} else {
+			nstr.WriteString(string(str[i]))
+		}
+	}
+	return len(str), len(nstr.String()) + 2
 }
 
 func (s *Server) Solve2015day8part1(ctx context.Context) (*pb.SolveResponse, error) {
@@ -41,4 +55,23 @@ func (s *Server) Solve2015day8part1(ctx context.Context) (*pb.SolveResponse, err
 	}
 
 	return &pb.SolveResponse{Answer: int32(chs - cds)}, nil
+}
+
+func (s *Server) Solve2015day8part2(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2015-8.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	trimmed := strings.TrimSpace(data)
+
+	chs := 0
+	cds := 0
+	for _, str := range strings.Split(trimmed, "\n") {
+		c1, c2 := computeStringAddition(str)
+		chs += c1
+		cds += c2
+	}
+
+	return &pb.SolveResponse{Answer: int32(cds - chs)}, nil
 }
--- 2015-8_test.go
+++ 2015-8_test.go
@@ -26,3 +26,26 @@ func TestDay20158P1(t *testing.T) {
 		}
 	}
 }
+
+func TestDay20158P2(t *testing.T) {
+	cases := []struct {
+		in         string
+		characters int
+		codes      int
+	}{
+		{`""`, 2, 6},
+		{`"abc"`, 5, 9},
+		{`"aaa\"aaa"`, 10, 16},
+		{`"\x27"`, 6, 11},
+	}
+
+	for _, c := range cases {
+		chs, cds := computeStringAddition(c.in)
+		if chs != c.characters {
+			t.Fatalf("Wrong number of characters on %v(%v should have been %v)", c.in, chs, c.characters)
+		}
+		if cds != c.codes {
+			t.Fatalf("Wrong string length on %v (%v should have been %v)", c.in, cds, c.codes)
+		}
+	}
+}
