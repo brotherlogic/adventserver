fa1154f83e3bad2fb6a56fbdf9786df7d2751e80
Simon Tucker <brotherlogic@gmail.com>
2022-09-30 06:52:58 -0700

    Log on run

--- 2015-9_test.go
+++ 2015-9_test.go
@@ -1,13 +1,17 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
 
 func Test2015Day9Part1(t *testing.T) {
 	details := `London to Dublin = 464
 	London to Belfast = 518
 	Dublin to Belfast = 141`
 
-	result := computeBestDistance(details)
+	server := Init()
+	result := server.computeBestDistance(context.Background(), details)
 	if result != 605 {
 		t.Errorf("Wrong result %v vs 605", result)
 	}
