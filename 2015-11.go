f2d9d6d2347f206439003709792b8912c193eac3
Simon Tucker <brotherlogic@gmail.com>
2022-10-01 08:08:33 -0700

    Added part 1

---
+++ 2015-11.go
@@ -0,0 +1,21 @@
+package main
+
+func incrementString(s string) string {
+	return s
+}
+
+func isValidSantaPassword(s string) bool {
+	return hasTrio(s) && noBadChars(s) && twoPairs(zz)
+}
+
+func hasTrio(s string) bool {
+	return false
+}
+
+func noBadChars(s string) bool {
+	return false
+}
+
+func twoPairs(s string) bool {
+	return false
+}
---
+++ 2015-11_test.go
@@ -0,0 +1,45 @@
+package main
+
+import "testing"
+
+func TestIncrementString(t *testing.T) {
+	cases := []struct {
+		in  string
+		out string
+	}{
+		{"xx", "xy"},
+		{"xy", "xz"},
+		{"xz", "ya"},
+		{"ya", "yb"},
+		{"yb", "yc"},
+	}
+
+	for _, c := range cases {
+		out := incrementString(c.in)
+		if out != c.out {
+			t.Fatalf("Bad trans %v => %v (should have been %v)", c.in, out, c.out)
+		}
+	}
+}
+
+func TestBadPass(t *testing.T) {
+	cases := []struct {
+		in  string
+		out bool
+	}{
+		{"hijklmmn", false},
+		{"abbceffg", false},
+		{"abbcegjk", false},
+		{"abcdefgh", true},
+		{"abcdffaa", true},
+		{"ghijklmn", true},
+		{"ghjaabcc", true},
+	}
+
+	for _, c := range cases {
+		out := isValidSantaPassword(c.in)
+		if out != c.out {
+			t.Fatalf("Bad trans %v => %v (should have been %v)", c.in, out, c.out)
+		}
+	}
+}
