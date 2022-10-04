439763b83f99b0edc6e1291f779157e7d7199a3f
Simon Tucker <brotherlogic@gmail.com>
2022-10-04 06:24:57 -0700

    Bug fixes

--- 2015-14_test.go
+++ 2015-14_test.go
@@ -1,11 +1,13 @@
 package main
 
-func TestFullP1(t *testing.T)){
-	data :=`Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+import "testing"
+
+func TestFullP1(t *testing.T) {
+	data := `Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
 	Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.`
 
 	winner := getDistance(data, 1000)
 	if winner != 1120 {
-		t.Errorf("Bad Distance: %v should have been 1120", distance)
+		t.Errorf("Bad Distance: %v should have been 1120", winner)
 	}
 }
