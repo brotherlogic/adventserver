53470baea0bccb187c095cba537d42bad205ce72
Simon Tucker <brotherlogic@gmail.com>
2022-10-04 06:40:53 -0700

    Made to pass

--- 2015-14.go
+++ 2015-14.go
@@ -2,13 +2,51 @@ package main
 
 import (
 	"context"
+	"strconv"
 	"strings"
 
 	pb "github.com/brotherlogic/adventserver/proto"
 )
 
+type reindeer struct {
+	speed     int
+	blastTime int
+	waitTime  int
+}
+
+func getRein(line string) *reindeer {
+	elems := strings.Fields(line)
+	spd, _ := strconv.ParseInt(elems[3], 10, 32)
+	blst, _ := strconv.ParseInt(elems[6], 10, 32)
+	wait, _ := strconv.ParseInt(elems[13], 10, 32)
+
+	return &reindeer{speed: int(spd), blastTime: int(blst), waitTime: int(wait)}
+}
+
+func computeDistance(r *reindeer, time int) int {
+	segments := time / (r.blastTime + r.waitTime)
+	remainder := time % (r.blastTime + r.waitTime)
+
+	little := 0
+	if remainder < r.blastTime {
+		little = (r.speed * remainder)
+	} else {
+		little = r.speed * r.blastTime
+	}
+
+	return little + segments*(r.speed*r.blastTime)
+}
+
 func getDistance(data string, time int) int32 {
-	return int32(0)
+	bd := 0
+	for _, line := range strings.Split(data, "\n") {
+		b := computeDistance(getRein(line), time)
+		if b > bd {
+			bd = b
+		}
+	}
+
+	return int32(bd)
 }
 
 func (s *Server) Solve2015day14part1(ctx context.Context) (*pb.SolveResponse, error) {
--- 2015-14_test.go
+++ 2015-14_test.go
@@ -11,3 +11,25 @@ func TestFullP1(t *testing.T) {
 		t.Errorf("Bad Distance: %v should have been 1120", winner)
 	}
 }
+
+func TestPartialDay14(t *testing.T) {
+	cases := []struct {
+		spd      int
+		blast    int
+		wait     int
+		time     int
+		distance int
+	}{
+		{14, 10, 127, 1, 14},
+		{16, 11, 162, 1, 16},
+		{14, 10, 127, 11, 140},
+		{16, 11, 162, 11, 176},
+	}
+
+	for _, c := range cases {
+		distance := computeDistance(&reindeer{speed: c.spd, blastTime: c.blast, waitTime: c.wait}, c.time)
+		if distance != c.distance {
+			t.Errorf("%+v @ %v -> %v (should have been %v", &reindeer{speed: c.spd, blastTime: c.blast, waitTime: c.wait}, c.time, distance, c.distance)
+		}
+	}
+}
