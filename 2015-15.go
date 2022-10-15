b447987b4e62dd1074329b078f836d99624be341
Simon Tucker <brotherlogic@gmail.com>
2022-10-14 17:44:32 -0700

    Added shell for part 1

---
+++ 2015-15.go
@@ -0,0 +1,5 @@
+package main
+
+func computeBestScore(data string) int {
+	return 0
+}
---
+++ 2015-15_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+	data := `Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+	Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3`
+
+	bestScore := computeBestScore(data)
+
+	if bestScore != 62842880 {
+		t.Errorf("Score should have been 62842880, was %v", bestScore)
+	}
+}
