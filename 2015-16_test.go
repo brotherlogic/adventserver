e80ed91027219c011002050638a341d275633f08
Simon Tucker <brotherlogic@gmail.com>
2022-10-20 07:18:37 -0700

    Added logic for part 1

---
+++ 2015-16_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Test2015_16_1(t *testing.T) {
+	known := properties{
+		children:    3,
+		cats:        7,
+		samoyeds:    2,
+		pomeranians: 3,
+		akitas:      0,
+		vizslas:     0,
+		goldfish:    5,
+		trees:       3,
+		cars:        2,
+		perfumes:    1,
+	}
+
+	aunt := findAunt("Sue 1: cars: 9, akitas: 3, goldfish: 0", known)
+
+	if aunt != 0 {
+		t.Errorf("Found aunt?")
+	}
+}
