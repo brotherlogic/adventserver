733bbfb9c80bb9db81715e91f7b0659a2c33c5d7
Simon Tucker <brotherlogic@gmail.com>
2022-10-27 11:27:35 -0700

    Fix error in return

--- adventserverapi.go
+++ adventserverapi.go
@@ -19,7 +19,7 @@ var (
 )
 
 func (s *Server) Solve(ctx context.Context, req *pb.SolveRequest) (*pb.SolveResponse, error) {
-	if reflect.ValueOf(s).MethodByName(fmt.Sprintf("Solve%vday%vpart%v", req.GetYear(), req.GetDay(), req.GetPart())).IsZero() {
+	if !reflect.ValueOf(s).MethodByName(fmt.Sprintf("Solve%vday%vpart%v", req.GetYear(), req.GetDay(), req.GetPart())).IsValid() {
 		return nil, fmt.Errorf("cannot find %v", reflect.ValueOf(s).MethodByName(fmt.Sprintf("Solve%vday%vpart%v", req.GetYear(), req.GetDay(), req.GetPart())))
 
 	}
