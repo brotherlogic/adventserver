a64b355119ec40b672bb44961ee1c2b981594e1e
Simon Tucker <brotherlogic@gmail.com>
2022-11-18 18:17:38 -0800

    Oops

--- 2015-19.go
+++ 2015-19.go
@@ -120,7 +120,7 @@ func (s *Server) Solve2015day19part1(ctx context.Context) (*pb.SolveResponse, er
 	return &pb.SolveResponse{Answer: int32(getMolecules(trimmed))}, nil
 }
 
-func (s *Server) Solve2015day19part12(ctx context.Context) (*pb.SolveResponse, error) {
+func (s *Server) Solve2015day19part2(ctx context.Context) (*pb.SolveResponse, error) {
 	data, err := s.loadFile(ctx, "/media/scratch/advent/2015-19.txt")
 	if err != nil {
 		return nil, err
