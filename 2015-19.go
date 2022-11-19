c9fd625f84aa0d650990ab5d21cdd95afa486a78
Simon Tucker <brotherlogic@gmail.com>
2022-11-18 18:06:34 -0800

    Add part 2

--- 2015-19.go
+++ 2015-19.go
@@ -119,3 +119,14 @@ func (s *Server) Solve2015day19part1(ctx context.Context) (*pb.SolveResponse, er
 
 	return &pb.SolveResponse{Answer: int32(getMolecules(trimmed))}, nil
 }
+
+func (s *Server) Solve2015day19part12(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2015-19.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	trimmed := strings.TrimSpace(data)
+
+	return &pb.SolveResponse{Answer: int32(treeMolecules(trimmed))}, nil
+}
