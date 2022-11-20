e0db52327652bf8706e3e27db12a40e7eb9d0058
Simon Tucker <brotherlogic@gmail.com>
2022-11-20 15:20:21 -0800

    Adds in part 2

--- 2015-21.go
+++ 2015-21.go
@@ -85,6 +85,60 @@ func runFight(ehitp, edamage, earm int) int {
 	return best
 }
 
+func runBackwardsFight(ehitp, edamage, earm int) int {
+	weapons := []adjust{
+		{8, 4, 0},
+		{10, 5, 0},
+		{25, 6, 0},
+		{40, 7, 0},
+		{74, 8, 0},
+	}
+	armor := []adjust{
+		{0, 0, 0},
+		{13, 0, 1},
+		{31, 0, 2},
+		{53, 0, 3},
+		{75, 0, 4},
+		{102, 0, 5},
+	}
+	rings1 := []adjust{
+		{0, 0, 0},
+		{25, 1, 0},
+		{50, 2, 0},
+		{100, 3, 0},
+	}
+	rings2 := []adjust{
+		{0, 0, 0},
+		{20, 0, 1},
+		{40, 0, 2},
+		{80, 0, 3},
+	}
+
+	best := 0
+	for _, w := range weapons {
+		for _, a := range armor {
+			for _, r1 := range rings1 {
+				for _, r2 := range rings2 {
+					me := player{hitp: 100,
+						damage: w.damage + r1.damage,
+						armor:  a.armor + r2.armor}
+					if !fight(me, player{hitp: ehitp, damage: edamage, armor: earm}) {
+						if w.cost+a.cost+r1.cost+r2.cost > best {
+							best = w.cost + a.cost + r1.cost + r2.cost
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return best
+}
+
 func (s *Server) Solve2015day21part1(ctx context.Context) (*pb.SolveResponse, error) {
 	return &pb.SolveResponse{Answer: int32(runFight(103, 9, 2))}, nil
+}
+
+func (s *Server) Solve2015day21part2(ctx context.Context) (*pb.SolveResponse, error) {
+	return &pb.SolveResponse{Answer: int32(runBackwardsFight(103, 9, 2))}, nil
 }
