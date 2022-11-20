45afd1437282c5c3c401cba86854d197e769ebba
Simon Tucker <brotherlogic@gmail.com>
2022-11-20 14:58:51 -0800

    Added surroundings

--- 2015-21.go
+++ 2015-21.go
@@ -1,9 +1,22 @@
 package main
 
+import (
+	pb "github.com/brotherlogic/adventserver/proto"
+	"golang.org/x/net/context"
+)
+
 type player struct {
 	hitp, armor, damage int
 }
 
 func fight(p1, p2 player) bool {
 	return false
+}
+
+func runFight(hitp, damage, armor int) int {
+	return 0
+}
+
+func (s *Server) Solve2015day21part1(ctx context.Context) (*pb.SolveResponse, error) {
+	return &pb.SolveResponse{Answer: int32(runFight(103, 9, 2))}, nil
 }
