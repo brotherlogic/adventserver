a454592b0eb859ed6753f86dd3d0dc438d58b06f
Simon Tucker <brotherlogic@gmail.com>
2022-11-21 09:46:53 -0800

    Hmmm

--- 2015-22_test.go
+++ 2015-22_test.go
@@ -3,11 +3,12 @@ package main
 import "testing"
 
 func Test2015Day22Part1(t *testing.T) {
-	if !magicFight(player{hitp: 10, mana: 250}, player{hitp: 13, damage: 8}) {
-		t.Errorf("Bad fight for num 1")
+	val := magicFight(player{hitp: 10, mana: 250}, player{hitp: 13, damage: 8})
+	if val != 126 {
+		t.Errorf("Bad fight for num 1: %v", val)
 	}
 
-	if !magicFight(player{hitp: 10, mana: 250}, player{hitp: 14, damage: 8}) {
-		t.Errorf("Bad fight for ")
+	if magicFight(player{hitp: 10, mana: 250}, player{hitp: 14, damage: 8}) != 126 {
+		t.Errorf("Bad fight for num 2: %v", magicFight(player{hitp: 10, mana: 250}, player{hitp: 14, damage: 8}))
 	}
 }
