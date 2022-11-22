bda593a0748b3cebf0526cfd0a6fd85741cd5102
Simon Tucker <brotherlogic@gmail.com>
2022-11-22 15:23:18 -0800

    Fixed

--- 2015-24.go
+++ 2015-24.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"strconv"
 	"strings"
 
@@ -9,7 +10,66 @@ import (
 )
 
 func computeGrouping(weights []int) int {
-	return 0
+
+	sumv := 0
+	for _, weight := range weights {
+		sumv += weight
+	}
+	goal := sumv / 3
+
+	var g1, g2, g3 []int
+	res, _ := placeWeights(weights, g1, g2, g3, goal)
+
+	return res
+}
+
+func prod(v []int) int {
+	prodv := 1
+	for _, val := range v {
+		prodv *= val
+	}
+	return prodv
+}
+
+func sum(v []int) int {
+	prodv := 0
+	for _, val := range v {
+		prodv += val
+	}
+	return prodv
+}
+
+func placeWeights(weights, g1, g2, g3 []int, goal int) (int, int) {
+	if sum(g1) > goal || sum(g2) > goal || sum(g3) > goal {
+		return math.MaxInt, math.MaxInt
+	}
+
+	if len(weights) == 0 {
+		return prod(g1), len(g1)
+	}
+
+	bestv := math.MaxInt
+	bestl := math.MaxInt
+
+	resp1, len1 := placeWeights(weights[1:], append(g1, weights[0]), g2, g3, goal)
+	if len1 < bestl || (len1 == bestl && resp1 < bestv) {
+		bestv = resp1
+		bestl = len1
+	}
+
+	resp2, len2 := placeWeights(weights[1:], g1, append(g2, weights[0]), g3, goal)
+	if len2 < bestl || (len2 == bestl && resp2 < bestv) {
+		bestv = resp2
+		bestl = len2
+	}
+
+	resp3, len3 := placeWeights(weights[1:], g1, g2, append(g3, weights[0]), goal)
+	if len3 < bestl || (len3 == bestl && resp3 < bestv) {
+		bestv = resp3
+		bestl = len3
+	}
+
+	return bestv, bestl
 }
 
 func (s *Server) Solve2015day24part1(ctx context.Context) (*pb.SolveResponse, error) {
--- 2015-24_test.go
+++ 2015-24_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func Test2015_24_1(t *testing.T) {
-	weights := []int{1, 2, 3, 4, 5, 7, 11}
+	weights := []int{1, 2, 3, 4, 5, 7, 8, 9, 10, 11}
 	best := computeGrouping(weights)
 
 	if best != 99 {
