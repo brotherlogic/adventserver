bbb4d076415d3a88e51e3aea8c15402ac883c42d
Simon Tucker <brotherlogic@gmail.com>
2022-11-23 02:22:07 -0800

    Fixed to pass

--- 2015-25.go
+++ 2015-25.go
@@ -5,8 +5,24 @@ import (
 	"golang.org/x/net/context"
 )
 
+func getTriangleIndex(row, col int) int {
+	top := (col * (col + 1)) / 2
+	for i := 0; i < row-1; i++ {
+		top = top + (col + i)
+	}
+	return top
+}
+
 func convertCode(base, row, col int) int {
-	return 0
+	nth := getTriangleIndex(row, col)
+
+	currv := base
+	for i := 1; i < nth; i++ {
+		currv *= 252533
+		currv = currv % 33554393
+	}
+
+	return currv
 }
 
 func (s *Server) Solve2015day25part1(ctx context.Context) (*pb.SolveResponse, error) {
