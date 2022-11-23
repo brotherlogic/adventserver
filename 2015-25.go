3662df6e8f19c2861adfbca1cf0a4522a1dd3a3f
Simon Tucker <brotherlogic@gmail.com>
2022-11-23 02:11:55 -0800

    Adds response part

--- 2015-25.go
+++ 2015-25.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	pb "github.com/brotherlogic/adventserver/proto"
+	"golang.org/x/net/context"
+)
+
 func convertCode(base, row, col int) int {
 	return 0
+}
+
+func (s *Server) Solve2015day25part1(ctx context.Context) (*pb.SolveResponse, error) {
+	return &pb.SolveResponse{BigAnswer: int64(convertCode(20151125, 2981, 3075))}, nil
 }
