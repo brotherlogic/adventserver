d9d43da5fd2bcb0f1cf1be2c5e52a40cf26b74b4
Simon Tucker <brotherlogic@gmail.com>
2022-11-22 21:15:00 -0800

    Fixes

--- 2015-24_test.go
+++ 2015-24_test.go
@@ -1,25 +1,22 @@
 package main
 
 import (
-	"log"
 	"testing"
 )
 
 func Test2015_24_1(t *testing.T) {
 	weights := []int{1, 2, 3, 4, 5, 7, 8, 9, 10, 11}
-	best := computeGrouping(weights)
+	best := computeGrouping(weights, 3)
 
 	if best != 99 {
 		t.Errorf("Bad grouping: %v (99)", best)
 	}
 }
+func Test2015_24_2(t *testing.T) {
+	weights := []int{1, 2, 3, 4, 5, 7, 8, 9, 10, 11}
+	best := computeGrouping(weights, 4)
 
-func TestSup2015_24_1(t *testing.T) {
-	weights := []int{1, 2, 3, 5, 7, 13, 17, 19, 23, 29, 31, 37, 41, 43, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113}
-	log.Printf("LEN %v", len(weights))
-	best := computeGrouping(weights)
-
-	if best != 99 {
-		t.Errorf("Bad grouping: %v (99)", best)
+	if best != 44 {
+		t.Errorf("Bad grouping: %v (44)", best)
 	}
 }
