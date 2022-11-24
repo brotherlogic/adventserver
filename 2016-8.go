6d4fd45795cbe0d93afc60c628dfd9c2fae5eff3
Simon Tucker <brotherlogic@gmail.com>
2022-11-23 17:30:01 -0800

    Adds scaffolding

--- 2016-8.go
+++ 2016-8.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	pb "github.com/brotherlogic/adventserver/proto"
+	"golang.org/x/net/context"
+)
+
 func runLightProgram(x, y int, program string) [][]bool {
 	b := make([][]bool, y)
 	for i := range b {
@@ -7,4 +12,25 @@ func runLightProgram(x, y int, program string) [][]bool {
 	}
 
 	return b
+}
+
+func countBoolArr(b [][]bool) int {
+	count := 0
+	for x := range b {
+		for y := range b[x] {
+			if b[x][y] {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func (s *Server) Solve2016day8part1(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2016-8.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.SolveResponse{Answer: int32(countBoolArr(runLightProgram(50, 6, data)))}, nil
 }
