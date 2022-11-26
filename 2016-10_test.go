7206cbb4b9fe7a1f437d06ce3d368e83456ad4fe
Simon Tucker <brotherlogic@gmail.com>
2022-11-25 21:21:18 -0800

    Added test for part 2

--- 2016-10_test.go
+++ 2016-10_test.go
@@ -44,3 +44,17 @@ func Test2016_10_1_Blanks(t *testing.T) {
 		t.Errorf("Program did not run correctly: %+v", res[0])
 	}
 }
+
+func Test2016_10_2(t *testing.T) {
+	data := `value 5 goes to bot 2
+	bot 2 gives low to bot 1 and high to bot 0
+	value 3 goes to bot 1
+	bot 1 gives low to output 1 and high to bot 0
+	bot 0 gives low to output 2 and high to output 0
+	value 2 goes to bot 2`
+
+	_, out := runBotProgram(data)
+	if out[0] != 5 {
+		t.Errorf("Program did not run correctly: %+v", out)
+	}
+}
