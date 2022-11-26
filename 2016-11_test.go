dcacc660e4554983a1243c820c1e1c541b249d01
Simon Tucker <brotherlogic@gmail.com>
2022-11-26 09:09:01 -0800

    2016, Day 11, Part 1

---
+++ 2016-11_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func Test2016_11_1(t *testing.T) {
+	data := `The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
+	The second floor contains a hydrogen generator.
+	The third floor contains a lithium generator.
+	The fourth floor contains nothing relevant.`
+
+	res := findFloors(data)
+	if res != 11 {
+		t.Errorf("Could not resolve program: %v (%v)", res, 11)
+	}
+}
