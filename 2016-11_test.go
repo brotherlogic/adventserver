e8c8db856b624e4f577fa3657bea9b3bb5d291df
Simon Tucker <brotherlogic@gmail.com>
2022-11-26 20:33:37 -0800

    Added failing test on the read

--- 2016-11_test.go
+++ 2016-11_test.go
@@ -8,8 +8,29 @@ func Test2016_11_1(t *testing.T) {
 	The third floor contains a lithium generator.
 	The fourth floor contains nothing relevant.`
 
-	res, _ := findFloors(data)
+	res, _, _ := findFloors(data)
 	if res != 11 {
 		t.Errorf("Could not resolve program: %v (%v)", res, 11)
+	}
+}
+
+func Test2016_11_1_read(t *testing.T) {
+	data := `The first floor contains a strontium generator, a strontium-compatible microchip, a plutonium generator, and a plutonium-compatible microchip.
+	The second floor contains a thulium generator, a ruthenium generator, a ruthenium-compatible microchip, a curium generator, and a curium-compatible microchip.
+	The third floor contains a thulium-compatible microchip.
+	The fourth floor contains nothing relevant.`
+
+	found := false
+	floors := buildFloors(data)
+	for _, f := range floors.floors {
+		for _, e := range f {
+			if e == "rM" {
+				found = true
+			}
+		}
+	}
+
+	if !found {
+		t.Errorf("Did not find rM: %+v", floors)
 	}
 }
