d923ad2893cd0e13dc16b485c641d0e848b193c2
Simon Tucker <brotherlogic@gmail.com>
2022-11-30 10:28:06 -0800

    Adds scaffolding for part 1 (#107)

---
+++ 2016-12.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"strings"
+
+	pb "github.com/brotherlogic/adventserver/proto"
+	"golang.org/x/net/context"
+)
+
+type mstate struct {
+	a, b, c, d int
+}
+
+func (m *mstate) set(reg string, value int) {
+	switch reg {
+	case "a":
+		m.a = value
+	case "b":
+		m.b = value
+	case "c":
+		m.c = value
+	case "d":
+		m.d = value
+	default:
+		log.Fatalf("Bad set: %v", reg)
+	}
+}
+
+func runMonorailProgram(data string) *mstate {
+
+	ppoint := 0
+	lines := strings.Split(data, "\n")
+	mstate := &mstate{}
+
+	for ppoint < len(lines) {
+		fields := strings.Fields(lines[ppoint])
+		switch fields[0] {
+		case "jnz":
+			val, _ := strconv.ParseInt(fields[2], 10, 32)
+			switch fields[1] {
+			case "a":
+				if mstate.a != 0 {
+					ppoint += int(val)
+				}
+			case "b":
+				if mstate.b != 0 {
+					ppoint += int(val)
+				}
+			case "c":
+				if mstate.c != 0 {
+					ppoint += int(val)
+				}
+			case "d":
+				if mstate.d != 0 {
+					ppoint += int(val)
+				}
+			}
+		case "cpy":
+			if fields[1] == "a" {
+				mstate.set(fields[2], mstate.a)
+			} else if fields[1] == "b" {
+				mstate.set(fields[2], mstate.b)
+			} else if fields[1] == "c" {
+				mstate.set(fields[2], mstate.b)
+			} else if fields[1] == "d" {
+				mstate.set(fields[2], mstate.b)
+			} else {
+				val, _ := strconv.ParseInt(fields[1], 10, 32)
+				mstate.set(fields[2], int(val))
+			}
+			ppoint++
+		case "inc":
+			switch fields[1] {
+			case "a":
+				mstate.a++
+			case "b":
+				mstate.b++
+			case "c":
+				mstate.c++
+			case "d":
+				mstate.d++
+			}
+			ppoint++
+		case "dec":
+			switch fields[1] {
+			case "a":
+				mstate.a--
+			case "b":
+				mstate.b--
+			case "c":
+				mstate.c--
+			case "d":
+				mstate.d--
+			}
+			ppoint++
+		}
+	}
+
+	return mstate
+}
+
+func (s *Server) Solve2016day12part1(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2016-12.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	state := runMonorailProgram(data)
+
+	return &pb.SolveResponse{Answer: int32(state.a)}, nil
+}
---
+++ 2016-12_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+func Test2016_12_1(t *testing.T) {
+	data := `cpy 41 a
+	inc a
+	inc a
+	dec a
+	jnz a 2
+	dec a`
+
+	state := runMonorailProgram(data)
+
+	if state.a != 42 {
+		t.Errorf("Bad program state: %+v", state)
+	}
+}
