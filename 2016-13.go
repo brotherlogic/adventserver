c04668d605b623d6d72b9587693b65ad2d491307
Simon Tucker <brotherlogic@gmail.com>
2022-11-30 18:08:32 -0800

    Ran the search

--- 2016-13.go
+++ 2016-13.go
@@ -1,11 +1,91 @@
 package main
 
 import (
+	"strconv"
+
 	pb "github.com/brotherlogic/adventserver/proto"
 	"golang.org/x/net/context"
 )
 
+type mazePoint struct {
+	x, y int
+	best int
+}
+
+func isWall(key, x, y int) bool {
+	num := x*x + 3*x + 2*x*y + y + y*y
+	num += key
+	binary := strconv.FormatInt(int64(num), 2)
+	count := 0
+	for _, char := range binary {
+		if char == '1' {
+			count++
+		}
+	}
+
+	return count%2 == 1
+}
+
+func isLegitMove(key int, seen map[int]map[int]int, x, y int) bool {
+	if isWall(key, x, y) {
+		return false
+	}
+
+	if val, ok := seen[x]; ok {
+		if _, aok := val[y]; aok {
+			return false
+		}
+	}
+
+	return true
+}
+
 func runMaze(key, x, y int) int {
+	var queue []*mazePoint
+	queue = append(queue, &mazePoint{x: 1, y: 1, best: 0})
+
+	seen := make(map[int]map[int]int)
+	seen[1] = make(map[int]int)
+	seen[1][1] = 0
+
+	for {
+		head := queue[0]
+		queue = queue[1:]
+
+		// Look for a win
+		if head.x == x && head.y == y {
+			return head.best
+		}
+
+		// Generate all other locations
+		if isLegitMove(key, seen, head.x+1, head.y) {
+			queue = append(queue, &mazePoint{head.x + 1, head.y, head.best + 1})
+			if _, ok := seen[head.x+1]; !ok {
+				seen[head.x+1] = make(map[int]int)
+			}
+			seen[head.x+1][head.y] = head.best + 1
+		}
+
+		if isLegitMove(key, seen, head.x-1, head.y) {
+			queue = append(queue, &mazePoint{head.x - 1, head.y, head.best + 1})
+			if _, ok := seen[head.x-1]; !ok {
+				seen[head.x-1] = make(map[int]int)
+			}
+			seen[head.x-1][head.y] = head.best + 1
+
+		}
+
+		if isLegitMove(key, seen, head.x, head.y+1) {
+			queue = append(queue, &mazePoint{head.x, head.y + 1, head.best + 1})
+			seen[head.x][head.y+1] = head.best + 1
+		}
+
+		if isLegitMove(key, seen, head.x, head.y-1) {
+			queue = append(queue, &mazePoint{head.x, head.y - 1, head.best + 1})
+			seen[head.x][head.y-1] = head.best + 1
+		}
+	}
+
 	return 0
 }
 
