172ad4488864846690501ecd04dd8042105af14c
Simon Tucker <brotherlogic@gmail.com>
2022-12-01 09:24:42 -0800

    Cleared errors

--- 2022-1.go
+++ 2022-1.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"strconv"
+	"strings"
+
 	pb "github.com/brotherlogic/adventserver/proto"
 	"golang.org/x/net/context"
 )
 
 func countCalories(data string) int {
-	return 0
+
+	best := 0
+
+	sofar := 0
+	for _, line := range strings.Split(data, "\n") {
+		if len(strings.TrimSpace(line)) == 0 {
+			if sofar > best {
+				best = sofar
+			}
+			sofar = 0
+		} else {
+			val, _ := strconv.ParseInt(strings.TrimSpace(line), 10, 32)
+			sofar += int(val)
+		}
+	}
+
+	return best
 }
 
 func (s *Server) Solve2022day22part1(ctx context.Context) (*pb.SolveResponse, error) {
