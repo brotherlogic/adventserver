98567d6db1bbb918f74ead77f3eca395750f272e
Simon Tucker <brotherlogic@gmail.com>
2022-12-01 15:30:40 -0800

    Runs part2 (#120)

--- 2016-14_test.go
+++ 2016-14_test.go
@@ -6,16 +6,23 @@ import (
 	"testing"
 )
 
-func Test2016_14_1(t *testing.T) {
-	indexes := buildKeys("abc")
+func Test2016_14_2(t *testing.T) {
+	indexes := buildKeys("abc", true)
 
-	if indexes[64] != 22728 {
+	if indexes[64] != 22551 {
 		t.Errorf("Bad Key: %v", indexes[64])
 	}
 }
 
+func Test2016Comp_14_2(t *testing.T) {
+	key := makeKey("abc", 0, true)
+	if key != "a107ff634856bb300138cac6568c0f24" {
+		t.Errorf("Bad key: %v", key)
+	}
+}
+
 func Test2016Sup_14_1(t *testing.T) {
-	key := makeKey("abc", 18)
+	key := makeKey("abc", 18, false)
 	if !strings.Contains(key, "cc38887a5") {
 		t.Errorf("Bad key: %v", key)
 	}
@@ -32,7 +39,7 @@ func Test2016Sup_14_1(t *testing.T) {
 }
 
 func Test2016SupFive_14_1(t *testing.T) {
-	key := makeKey("abc", 39)
+	key := makeKey("abc", 39, false)
 
 	threes := countThrees(key)
 	found := false
@@ -44,7 +51,7 @@ func Test2016SupFive_14_1(t *testing.T) {
 		t.Errorf("No e: %v", threes)
 	}
 
-	key2 := makeKey("abc", 816)
+	key2 := makeKey("abc", 816, false)
 	fives := countFives(key2)
 	found = false
 	for _, val := range fives {
@@ -61,7 +68,7 @@ func Test2016SupFive_14_1(t *testing.T) {
 func Test2016SupKey_14_1(t *testing.T) {
 	fives := make(map[string][]int)
 
-	threes, _ := buildKey("abc", 39, fives)
+	threes, _ := buildKey("abc", 39, fives, false)
 
 	found := false
 	if threes == "e" {
@@ -89,7 +96,7 @@ func Test2016SupKey_14_1(t *testing.T) {
 }
 
 func Test2016SupDiff(t *testing.T) {
-	f := findFives("abc")
+	f := findFives("abc", false)
 	if f != 22728 {
 		t.Errorf("Bad: %v", f)
 	}
@@ -98,8 +105,8 @@ func Test2016SupDiff(t *testing.T) {
 func Test2016SupKeyLast_14_1(t *testing.T) {
 	fives := make(map[string][]int)
 
-	key := makeKey("abc", 22728)
-	threes, f := buildKey("abc", 22728, fives)
+	key := makeKey("abc", 22728, false)
+	threes, f := buildKey("abc", 22728, fives, false)
 	log.Printf("F %v", f)
 	fives = f
 
