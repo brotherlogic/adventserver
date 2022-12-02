a6f9149aa9c662e714ea5b6761c4cb79c98b6829
Simon Tucker <brotherlogic@gmail.com>
2022-12-02 09:12:59 -0800

    Adds part 2

--- 2022-2_test.go
+++ 2022-2_test.go
@@ -13,3 +13,15 @@ func Test2022_2_1(t *testing.T) {
 		t.Errorf("Bad RPS Score: %v (15)", score)
 	}
 }
+
+func Test2022_2_2(t *testing.T) {
+	data := `A Y
+	B X
+	C Z`
+
+	score := getRPSScoreAmend(data)
+
+	if score != 12 {
+		t.Errorf("Bad RPS Score: %v (15)", score)
+	}
+}
