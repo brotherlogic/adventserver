23b93147241bc5f2c04eb06fd94741c4c9dc5a9b
Simon Tucker <brotherlogic@gmail.com>
2022-12-04 08:34:41 -0800

    Fixed tests to pass

--- 2022-4.go
+++ 2022-4.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"strconv"
+	"strings"
+
 	pb "github.com/brotherlogic/adventserver/proto"
 	"golang.org/x/net/context"
 )
 
 func countFullyOverlapping(data string) int {
-	return 0
+	count := 0
+
+	for _, line := range strings.Split(data, "\n") {
+		if doesOverlap(line) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func getNums(piece string) []int {
+	elems := strings.Split(piece, "-")
+	num1, _ := strconv.ParseInt(elems[0], 10, 32)
+	num2, _ := strconv.ParseInt(elems[1], 10, 32)
+
+	return []int{int(num1), int(num2)}
 }
 
 func doesOverlap(line string) bool {
+	elems := strings.Split(line, ",")
+
+	nums1 := getNums(elems[0])
+	nums2 := getNums(elems[1])
+
+	if (nums1[0] <= nums2[0] && nums1[1] >= nums2[1]) ||
+		(nums2[0] <= nums1[0] && nums2[1] >= nums1[1]) {
+		return true
+	}
+
 	return false
 }
 
