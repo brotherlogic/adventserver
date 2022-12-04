e6fb3f2e53170bfc3b4c0f182da990eef225db62
Simon Tucker <brotherlogic@gmail.com>
2022-12-04 08:38:25 -0800

    Working on 2022, Day 4, Part 1 (#132)

---
+++ 2022-4.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+
+	pb "github.com/brotherlogic/adventserver/proto"
+	"golang.org/x/net/context"
+)
+
+func countFullyOverlapping(data string) int {
+	count := 0
+
+	for _, line := range strings.Split(data, "\n") {
+		if doesOverlap(line) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func getNums(piece string) []int {
+	elems := strings.Split(piece, "-")
+	num1, _ := strconv.ParseInt(elems[0], 10, 32)
+	num2, _ := strconv.ParseInt(elems[1], 10, 32)
+
+	return []int{int(num1), int(num2)}
+}
+
+func doesOverlap(line string) bool {
+	elems := strings.Split(line, ",")
+
+	nums1 := getNums(elems[0])
+	nums2 := getNums(elems[1])
+
+	if (nums1[0] <= nums2[0] && nums1[1] >= nums2[1]) ||
+		(nums2[0] <= nums1[0] && nums2[1] >= nums1[1]) {
+		return true
+	}
+
+	return false
+}
+
+func (s *Server) Solve2022da43part1(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2022-4.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.SolveResponse{Answer: int32(countFullyOverlapping(data))}, nil
+}
---
+++ 2022-4_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func Test2022_4_1(t *testing.T) {
+	data := `2-4,6-8
+	2-3,4-5
+	5-7,7-9
+	2-8,3-7
+	6-6,4-6
+	2-6,4-8`
+
+	count := countFullyOverlapping(data)
+	if count != 2 {
+		t.Errorf("Not the right overlap: %v (2)", count)
+	}
+}
+
+func Test2022_4_1_Overlap(t *testing.T) {
+	tests := []struct {
+		in      string
+		overlap bool
+	}{
+		{"2-8,3-7", true},
+		{"6-6,4-6", true},
+		{"2-6,4-8", false},
+		{"2-4,6-8", false},
+	}
+
+	for _, test := range tests {
+		overlap := doesOverlap(test.in)
+		if overlap != test.overlap {
+			t.Errorf("Bad overlap determineation: %v -> %v / %v", test.in, overlap, test.overlap)
+		}
+	}
+}
