78acc4206f3c8d290b279b0a93befad3059dd5cb
Simon Tucker <brotherlogic@gmail.com>
2022-12-04 10:13:00 -0800

    Fixed 2016, Day 19

--- 2016-19.go
+++ 2016-19.go
@@ -6,7 +6,39 @@ import (
 )
 
 func runPresents(num int) int {
-	return 0
+	elfNum := make([]int, num)
+	elfPres := make([]int, num)
+	for i := 0; i < num; i++ {
+		elfNum[i] = i + 1
+		elfPres[i] = 1
+	}
+
+	pointer := 0
+	for {
+		if len(elfNum) == 1 {
+			return elfNum[0]
+		}
+
+		if elfPres[pointer] == 0 {
+			var nElfNum []int
+			var nElfPres []int
+			for i := 0; i < len(elfNum); i++ {
+				if i != pointer {
+					nElfNum = append(nElfNum, elfNum[i])
+					nElfPres = append(nElfPres, elfPres[i])
+				}
+			}
+
+			elfNum = nElfNum
+			elfPres = nElfPres
+		} else {
+			elfPres[pointer] += elfPres[(pointer+1)%len(elfPres)]
+			elfPres[(pointer+1)%len(elfPres)] = 0
+			pointer++
+			pointer = pointer % len(elfPres)
+		}
+	}
+
 }
 
 func (s *Server) Solve2016day19part1(ctx context.Context) (*pb.SolveResponse, error) {
