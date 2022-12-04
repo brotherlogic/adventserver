06ed30fd2597c96eb1af0b592b478dcbf2d19a94
Simon Tucker <brotherlogic@gmail.com>
2022-12-04 09:08:55 -0800

    Working on 2022, Day 4, Part 2

--- 2022-4.go
+++ 2022-4.go
@@ -20,6 +20,18 @@ func countFullyOverlapping(data string) int {
 	return count
 }
 
+func countOverlapping(data string) int {
+	count := 0
+
+	for _, line := range strings.Split(data, "\n") {
+		if doesOverlapEven(line) {
+			count++
+		}
+	}
+
+	return count
+}
+
 func getNums(piece string) []int {
 	elems := strings.Split(piece, "-")
 	num1, _ := strconv.ParseInt(elems[0], 10, 32)
@@ -45,6 +57,23 @@ func doesOverlap(line string) bool {
 	return false
 }
 
+func doesOverlapEven(line string) bool {
+	if len(strings.TrimSpace(line)) == 0 {
+		return false
+	}
+	elems := strings.Split(line, ",")
+
+	nums1 := getNums(elems[0])
+	nums2 := getNums(elems[1])
+
+	if (nums1[0] <= nums2[0] && nums1[1] >= nums2[1]) ||
+		(nums2[0] <= nums1[0] && nums2[1] >= nums1[1]) {
+		return true
+	}
+
+	return false
+}
+
 func (s *Server) Solve2022day4part1(ctx context.Context) (*pb.SolveResponse, error) {
 	data, err := s.loadFile(ctx, "/media/scratch/advent/2022-4.txt")
 	if err != nil {
@@ -52,4 +81,13 @@ func (s *Server) Solve2022day4part1(ctx context.Context) (*pb.SolveResponse, err
 	}
 
 	return &pb.SolveResponse{Answer: int32(countFullyOverlapping(data))}, nil
+}
+
+func (s *Server) Solve2022day4part2(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2022-4.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.SolveResponse{Answer: int32(countOverlapping(data))}, nil
 }
--- 2022-4_test.go
+++ 2022-4_test.go
@@ -16,6 +16,20 @@ func Test2022_4_1(t *testing.T) {
 	}
 }
 
+func Test2022_4_2(t *testing.T) {
+	data := `2-4,6-8
+	2-3,4-5
+	5-7,7-9
+	2-8,3-7
+	6-6,4-6
+	2-6,4-8`
+
+	count := countOverlapping(data)
+	if count != 4 {
+		t.Errorf("Not the right overlap: %v (2)", count)
+	}
+}
+
 func Test2022_4_1_Overlap(t *testing.T) {
 	tests := []struct {
 		in      string
@@ -30,6 +44,26 @@ func Test2022_4_1_Overlap(t *testing.T) {
 
 	for _, test := range tests {
 		overlap := doesOverlap(test.in)
+		if overlap != test.overlap {
+			t.Errorf("Bad overlap determineation: %v -> %v / %v", test.in, overlap, test.overlap)
+		}
+	}
+}
+
+func Test2022_4_2_Overlap(t *testing.T) {
+	tests := []struct {
+		in      string
+		overlap bool
+	}{
+		{"2-8,3-7", true},
+		{"6-6,4-6", true},
+		{"2-6,4-8", true},
+		{"2-4,6-8", false},
+		{"", false},
+	}
+
+	for _, test := range tests {
+		overlap := doesOverlapEven(test.in)
 		if overlap != test.overlap {
 			t.Errorf("Bad overlap determineation: %v -> %v / %v", test.in, overlap, test.overlap)
 		}
