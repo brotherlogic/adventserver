cdb25424b373295b4b2403154cfbc827ced407d0
Simon Tucker <brotherlogic@gmail.com>
2022-12-05 08:29:42 -0800

    Finished part 1

--- 2022-5_test.go
+++ 2022-5_test.go
@@ -4,14 +4,14 @@ import "testing"
 
 func Test2022_5_1(t *testing.T) {
 	data := `    [D]    
-	[N] [C]    
-	[Z] [M] [P]
-	 1   2   3 
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
 	
-	move 1 from 2 to 1
-	move 3 from 1 to 3
-	move 2 from 2 to 1
-	move 1 from 1 to 2`
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`
 
 	re := rearrangeCrates(data)
 
