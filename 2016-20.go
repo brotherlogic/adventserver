ff0b58fb8d34d1e1f7ccba7966388a989f08d82f
Simon Tucker <brotherlogic@gmail.com>
2022-12-05 16:04:26 -0800

    Working on 2016, Day 20, Part 1 (#145)

---
+++ 2016-20.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+
+	pb "github.com/brotherlogic/adventserver/proto"
+	"golang.org/x/net/context"
+)
+
+func ipOverlap(comp []int, st, en int) ([]int, bool) {
+	if st >= comp[0] && st <= comp[1] && en > comp[1] {
+		return []int{comp[0], en}, true
+	}
+
+	if en <= comp[1] && en >= comp[0] && st < comp[0] {
+		return []int{st, comp[1]}, true
+	}
+
+	return []int{}, false
+}
+
+func getLowIp(data string) int {
+	var ranges [][]int
+
+	for _, line := range strings.Split(data, "\n") {
+		if len(strings.TrimSpace(line)) > 0 {
+			elems := strings.Split(line, "-")
+			st, _ := strconv.ParseInt(elems[0], 10, 32)
+			en, _ := strconv.ParseInt(elems[1], 10, 32)
+
+			found := false
+			for i := 0; i < len(ranges); i++ {
+				if val, ok := ipOverlap(ranges[i], int(st), int(en)); ok {
+					ranges[i] = val
+					found = true
+					break
+				}
+			}
+			if !found {
+				ranges = append(ranges, []int{int(st), int(en)})
+			}
+		}
+	}
+
+	sort.SliceStable(ranges, func(i, j int) bool {
+		return ranges[i][0] < ranges[j][0]
+	})
+
+	return ranges[0][1] + 1
+}
+
+func (s *Server) Solve2016day20part1(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2016-20.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.SolveResponse{Answer: int32(getLowIp(data))}, nil
+}
