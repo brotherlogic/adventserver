6bd5effad08b0eab8d299d1107e6d28756d8336f
Simon Tucker <brotherlogic@gmail.com>
2022-12-06 10:29:33 -0800

    Working on 2022, Day 6, Part 2 (#147)

--- 2022-6.go
+++ 2022-6.go
@@ -5,11 +5,11 @@ import (
 	"golang.org/x/net/context"
 )
 
-func findMarker(str string) int {
+func findMarker(str string, cc int) int {
 	for i := 0; i < len(str)-3; i++ {
 		counts := make(map[byte]bool)
 		found := false
-		for j := i; j < i+4; j++ {
+		for j := i; j < i+cc; j++ {
 			if _, ok := counts[str[j]]; ok {
 				found = true
 				break
@@ -19,7 +19,7 @@ func findMarker(str string) int {
 		}
 
 		if !found {
-			return i + 4
+			return i + cc
 		}
 	}
 
@@ -32,5 +32,14 @@ func (s *Server) Solve2022day6part1(ctx context.Context) (*pb.SolveResponse, err
 		return nil, err
 	}
 
-	return &pb.SolveResponse{Answer: int32(findMarker(data))}, nil
+	return &pb.SolveResponse{Answer: int32(findMarker(data, 4))}, nil
+}
+
+func (s *Server) Solve2022day6part2(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2022-6.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.SolveResponse{Answer: int32(findMarker(data, 14))}, nil
 }
