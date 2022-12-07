aa18f6e2a9bdb6c938637073f19cd756a524c4db
Simon Tucker <brotherlogic@gmail.com>
2022-12-07 08:47:44 -0800

    Finished part 1

--- 2022-7.go
+++ 2022-7.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log"
+	"strconv"
+	"strings"
+
 	pb "github.com/brotherlogic/adventserver/proto"
 	"golang.org/x/net/context"
 )
@@ -11,17 +15,83 @@ type file struct {
 }
 
 type dir struct {
-	name  string
-	dirs  []*dir
-	files []*file
+	name   string
+	dirs   []*dir
+	files  []*file
+	parent *dir
 }
 
 func buildDirs(data string) *dir {
-	return &dir{name: "/", dirs: make([]*dir, 0), files: make([]*file, 0)}
+	head := &dir{name: "/", dirs: make([]*dir, 0), files: make([]*file, 0), parent: nil}
+
+	curr := head
+	for _, line := range strings.Split(strings.TrimSpace(data), "\n") {
+		fields := strings.Fields(line)
+		switch fields[0] {
+		case "$":
+			if fields[1] == "cd" {
+				if fields[2] == "/" {
+					curr = head
+					continue
+				}
+
+				if fields[2] == ".." {
+					curr = curr.parent
+					continue
+				}
+
+				found := false
+				for _, dd := range curr.dirs {
+					if dd.name == fields[2] {
+						curr = dd
+						found = true
+					}
+				}
+				if found {
+					continue
+				}
+
+				log.Fatalf("Unable to locate directory: %v", line)
+			}
+
+		case "dir":
+			curr.dirs = append(curr.dirs, &dir{name: fields[1], dirs: make([]*dir, 0), files: make([]*file, 0), parent: curr})
+
+		default:
+			size, _ := strconv.ParseInt(fields[0], 10, 32)
+			curr.files = append(curr.files, &file{name: fields[1], size: int(size)})
+		}
+	}
+
+	return head
 }
 
 func (d *dir) dirSum(limit int) int {
-	return 0
+	total := 0
+
+	for _, dd := range d.dirs {
+		if dd.size() <= limit {
+			total += dd.size()
+		}
+
+		total += dd.dirSum(limit)
+	}
+
+	return total
+}
+
+func (d *dir) size() int {
+
+	local := 0
+	for _, f := range d.files {
+		local += f.size
+	}
+
+	for _, dd := range d.dirs {
+		local += dd.size()
+	}
+
+	return local
 }
 
 func (s *Server) Solve2022day7part1(ctx context.Context) (*pb.SolveResponse, error) {
