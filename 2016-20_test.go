4a39eac01b296b1f1c08fc860b9118197cd7e566
Simon Tucker <brotherlogic@gmail.com>
2022-12-06 18:16:17 -0800

    more tests (#152)

--- 2016-20_test.go
+++ 2016-20_test.go
@@ -34,3 +34,63 @@ func Test2016_20_1_Sup2(t *testing.T) {
 		t.Errorf("Bad Low IP on biggy: %v (4793564)", low)
 	}
 }
+
+func Test2016_20_1_Inside(t *testing.T) {
+	data := `0-5
+2-3`
+	low := getLowIp(data)
+
+	if low != 6 {
+		t.Errorf("Bad Low IP on biggy: %v (6)", low)
+	}
+}
+
+func Test2016_20_1_Outside(t *testing.T) {
+	data := `2-3
+0-5`
+	low := getLowIp(data)
+
+	if low != 6 {
+		t.Errorf("Bad Low IP on biggy: %v (6)", low)
+	}
+}
+
+func Test2016_20_1_OverLeft(t *testing.T) {
+	data := `0-3
+2-5`
+	low := getLowIp(data)
+
+	if low != 6 {
+		t.Errorf("Bad Low IP on biggy: %v (6)", low)
+	}
+}
+
+func Test2016_20_1_OverRight(t *testing.T) {
+	data := `2-5
+0-3`
+	low := getLowIp(data)
+
+	if low != 6 {
+		t.Errorf("Bad Low IP on biggy: %v (6)", low)
+	}
+}
+
+func Test2016_20_1_TightLeft(t *testing.T) {
+	data := `0-3
+3-5`
+	low := getLowIp(data)
+
+	if low != 6 {
+		t.Errorf("Bad Low IP on biggy: %v (6)", low)
+	}
+}
+
+func Test2016_20_1_TightRight(t *testing.T) {
+	data := `3-5
+0-3`
+	low := getLowIp(data)
+
+	if low != 6 {
+		t.Errorf("Bad Low IP on biggy: %v (6)", low)
+	}
+}
