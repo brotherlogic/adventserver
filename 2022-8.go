4c466ee75c3cdf23b83e12ad85bd497a07651f67
Simon Tucker <brotherlogic@gmail.com>
2022-12-08 09:27:08 -0800

    Fixed to pass

--- 2022-8.go
+++ 2022-8.go
@@ -25,58 +25,69 @@ func buildTreeGrid(data string) [][]int {
 	return grid
 }
 
+func countSeen(grid [][]int, y, x int) int {
+	seenU := 0
+	for ny := y - 1; ny >= 0; ny-- {
+		if grid[ny][x] < grid[y][x] {
+			seenU++
+		} else {
+			seenU++
+			break
+		}
+	}
+	seenD := 0
+	for ny := y + 1; ny < len(grid); ny++ {
+		if grid[ny][x] < grid[y][x] {
+			seenD++
+		} else {
+			seenD++
+			break
+		}
+	}
+	seenL := 0
+	for nx := x - 1; nx >= 0; nx-- {
+		if grid[y][nx] < grid[y][x] {
+			seenL++
+		} else {
+			seenL++
+			break
+		}
+	}
+	seenR := 0
+	for nx := x + 1; nx < len(grid[0]); nx++ {
+		if grid[y][nx] < grid[y][x] {
+			seenR++
+		} else {
+			seenR++
+			break
+		}
+	}
+
+	return seenU * seenD * seenL * seenR
+}
+
 func bestTree(data string) int {
 	grid := buildTreeGrid(data)
-	var visible [][]bool
+	var see [][]int
 	for i := 0; i < len(grid); i++ {
-		var vis []bool
+		var s []int
 		for j := 0; j < len(grid[i]); j++ {
-			vis = append(vis, false)
+			s = append(s, 0)
 		}
-		visible = append(visible, vis)
+		see = append(see, s)
 	}
 
 	for y := 0; y < len(grid); y++ {
-		sofar := -1
 		for x := 0; x < len(grid[y]); x++ {
-			if grid[y][x] > sofar {
-				visible[y][x] = true
-				sofar = grid[y][x]
-			}
-		}
-
-		sofar = -1
-		for x := len(grid[y]) - 1; x > 0; x-- {
-			if grid[y][x] > sofar {
-				visible[y][x] = true
-				sofar = grid[y][x]
-			}
-		}
-	}
-
-	for x := 0; x < len(grid[0]); x++ {
-		sofar := -1
-		for y := 0; y < len(grid); y++ {
-			if grid[y][x] > sofar {
-				visible[y][x] = true
-				sofar = grid[y][x]
-			}
-		}
-
-		sofar = -1
-		for y := len(grid) - 1; y > 0; y-- {
-			if grid[y][x] > sofar {
-				visible[y][x] = true
-				sofar = grid[y][x]
-			}
+			see[y][x] = countSeen(grid, y, x)
 		}
 	}
 
 	count := 0
-	for i := 0; i < len(visible); i++ {
-		for j := 0; j < len(visible[i]); j++ {
-			if visible[i][j] {
-				count++
+	for i := 0; i < len(see); i++ {
+		for j := 0; j < len(see[i]); j++ {
+			if see[i][j] > count {
+				count = see[i][j]
 			}
 		}
 	}
