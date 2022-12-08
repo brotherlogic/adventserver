aaf547601e065be8ea24a45e976fa4c548314bd3
Simon Tucker <brotherlogic@gmail.com>
2022-12-07 16:47:18 -0800

    Working on 2016, Day 21, Part 1 (#158)

---
+++ 2016-21.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+
+	pb "github.com/brotherlogic/adventserver/proto"
+	"golang.org/x/net/context"
+)
+
+func replace(s1, s2, code string) string {
+	return strings.Replace(strings.Replace(strings.Replace(code, s1, "1", 1), s2, s1, 1), "1", s2, 1)
+}
+
+func reverse(s string) string {
+	rns := []rune(s)
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+	return string(rns)
+}
+
+func translateCode(code, trans string) string {
+	fields := strings.Fields(trans)
+	switch fields[0] {
+	case "swap":
+		if fields[1] == "position" {
+			p1, _ := strconv.ParseInt(fields[2], 10, 32)
+			p2, _ := strconv.ParseInt(fields[5], 10, 32)
+
+			return replace(string(code[int(p1)]), string(code[int(p2)]), code)
+		}
+		if fields[1] == "letter" {
+			return replace(fields[2], fields[5], code)
+		}
+	case "rotate":
+		if fields[1] == "left" {
+			count, _ := strconv.ParseInt(fields[2], 10, 32)
+			return code[count:] + code[:count]
+		}
+
+		if fields[1] == "right" {
+			count, _ := strconv.ParseInt(fields[2], 10, 32)
+			nv := len(code) - int(count)
+			return code[nv:] + code[:nv]
+		}
+
+		if fields[1] == "based" {
+			index := strings.Index(code, string(fields[6]))
+			if index >= 4 {
+				index++
+			}
+			ncode := code[len(code)-1:] + code[:len(code)-1]
+			return ncode[len(code)-index:] + ncode[:len(code)-index]
+		}
+	case "reverse":
+		p1, _ := strconv.ParseInt(fields[2], 10, 32)
+		p2, _ := strconv.ParseInt(fields[4], 10, 32)
+
+		return code[:p1] + reverse(code[p1:p2+1]) + code[p2+1:]
+	case "move":
+		p1, _ := strconv.ParseInt(fields[2], 10, 32)
+		p2, _ := strconv.ParseInt(fields[5], 10, 32)
+
+		val := string(code[p1])
+		nstr := code[:p1] + code[p1+1:]
+		return nstr[:p2] + val + nstr[p2:]
+
+	}
+	return code
+}
+
+func fullTranslate(data, code string) string {
+	for _, line := range strings.Split(data, "\n") {
+		if len(strings.TrimSpace(line)) > 0 {
+			code = translateCode(code, strings.TrimSpace(line))
+		}
+	}
+	return code
+}
+
+func (s *Server) Solve2016day21part1(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2016-21.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.SolveResponse{StringAnswer: fullTranslate(data, "abcdefgh")}, nil
+}
---
+++ 2016-21_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func Test2016_21_1(t *testing.T) {
+	tests := []struct {
+		in   string
+		code string
+		out  string
+	}{
+		{"abcde", "swap position 4 with position 0", "ebcda"},
+		{"ebcda", "swap letter d with letter b", "edcba"},
+		{"edcba", "reverse positions 0 through 4", "abcde"},
+		{"abcde", "rotate left 1 step", "bcdea"},
+		{"bcdea", "move position 1 to position 4", "bdeac"},
+		{"bdeac", "move position 3 to position 0", "abdec"},
+		{"abdec", "rotate based on position of letter b", "ecabd"},
+		{"ecabd", "rotate based on position of letter d", "decab"},
+	}
+
+	for _, test := range tests {
+		out := translateCode(test.in, test.code)
+		if out != test.out {
+			t.Errorf("Bad translate %v -> %v => %v (%v)", test.in, test.code, out, test.out)
+		}
+	}
+
+}
