59d4f615646bff8bf9031ba647a6cfe2c191a022
Simon Tucker <brotherlogic@gmail.com>
2022-12-10 08:30:51 -0800

    Add entry

--- 2022-10.go
+++ 2022-10.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	pb "github.com/brotherlogic/adventserver/proto"
+	"golang.org/x/net/context"
+)
+
 type elfProgram struct {
 	a      int
 	cycle  int
@@ -12,4 +17,15 @@ func (e elfProgram) getSignal() int {
 
 func runElfProgram(data string) elfProgram {
 	return elfProgram{}
+}
+
+func (s *Server) Solve2022day10part1(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2022-10.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	result := runElfProgram(data)
+
+	return &pb.SolveResponse{Answer: int32(result.getSignal())}, nil
 }
