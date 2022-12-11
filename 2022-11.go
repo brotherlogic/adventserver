6e28bceaa7e39d47bf44fc56e9947a7274e79881
Simon Tucker <brotherlogic@gmail.com>
2022-12-11 12:22:12 -0800

    Working on 2022, Day 11, Part 1 (#171)

---
+++ 2022-11.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+
+	pb "github.com/brotherlogic/adventserver/proto"
+	"golang.org/x/net/context"
+)
+
+type monkey struct {
+	number      int
+	items       []int
+	operation   string
+	adjustment  int
+	test        int
+	trueMonkey  int
+	falseMonkey int
+	seen        int
+}
+
+func buildMonkeys(data string) []*monkey {
+	var cmonkey *monkey
+	var monkeys []*monkey
+	for _, line := range strings.Split(data, "\n") {
+		nline := strings.TrimSpace(line)
+		if len(nline) > 0 {
+			fields := strings.Fields(nline)
+			switch fields[0] {
+			case "Monkey":
+				num, _ := strconv.ParseInt(fields[1][:len(fields[1])-1], 10, 32)
+				if num > 0 {
+					monkeys = append(monkeys, cmonkey)
+				}
+				cmonkey = &monkey{number: int(num), items: make([]int, 0)}
+
+			case "Starting":
+				tline := strings.Split(nline, ":")
+				nums := strings.Split(tline[1], ",")
+				for _, num := range nums {
+					numv, _ := strconv.ParseInt(strings.TrimSpace(num), 10, 32)
+					cmonkey.items = append(cmonkey.items, int(numv))
+				}
+			case "Operation:":
+				cmonkey.operation = fields[4]
+				num, _ := strconv.ParseInt(fields[5], 10, 32)
+				if num == 0 {
+					cmonkey.adjustment = -1
+				} else {
+					cmonkey.adjustment = int(num)
+				}
+			case "Test:":
+				num, _ := strconv.ParseInt(fields[3], 10, 32)
+				cmonkey.test = int(num)
+			case "If":
+				num, _ := strconv.ParseInt(fields[5], 10, 32)
+				if fields[1] == "true:" {
+					cmonkey.trueMonkey = int(num)
+				} else {
+					cmonkey.falseMonkey = int(num)
+				}
+			}
+		}
+	}
+
+	monkeys = append(monkeys, cmonkey)
+
+	return monkeys
+}
+
+func runMonkeys(monkeys []*monkey) {
+	mmap := make(map[int]*monkey)
+	for _, monkey := range monkeys {
+		mmap[monkey.number] = monkey
+	}
+
+	for _, monkey := range monkeys {
+		for len(monkey.items) > 0 {
+			vitem := monkey.items[0]
+			switch monkey.operation {
+			case "*":
+				if monkey.adjustment > 0 {
+					vitem *= monkey.adjustment
+				} else {
+					vitem = vitem * vitem
+				}
+			case "+":
+				if monkey.adjustment > 0 {
+					vitem += monkey.adjustment
+				} else {
+					vitem = vitem + vitem
+				}
+			}
+
+			vitem = vitem / 3
+
+			if vitem%monkey.test == 0 {
+				mmap[monkey.trueMonkey].items = append(monkeys[monkey.trueMonkey].items, vitem)
+			} else {
+				mmap[monkey.falseMonkey].items = append(monkeys[monkey.falseMonkey].items, vitem)
+			}
+			monkey.items = monkey.items[1:]
+			monkey.seen++
+		}
+
+	}
+}
+
+func getMonkeyTimes(monkeys []*monkey) []int {
+
+	var values []int
+	for _, m := range monkeys {
+		values = append(values, m.seen)
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(values)))
+
+	return values
+}
+
+func (s *Server) Solve2022day11part1(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2022-11.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	monkeys := buildMonkeys(data)
+	for i := 0; i < 20; i++ {
+		runMonkeys(monkeys)
+	}
+	vals := getMonkeyTimes(monkeys)
+
+	return &pb.SolveResponse{Answer: int32(vals[0] * vals[1])}, nil
+}
---
+++ 2022-11_test.go
@@ -0,0 +1,121 @@
+package main
+
+import "testing"
+
+func Test2022_11_1_Build(t *testing.T) {
+	data := `Monkey 0:
+	Starting items: 79, 98
+	Operation: new = old * 19
+	Test: divisible by 23
+	  If true: throw to monkey 2
+	  If false: throw to monkey 3`
+
+	monkeys := buildMonkeys(data)
+
+	if len(monkeys) != 1 {
+		t.Errorf("Too many monkeys: %v", len(monkeys))
+	}
+
+	if monkeys[0].number != 0 {
+		t.Errorf("Wrong number: %v", monkeys[0].number)
+	}
+
+	if len(monkeys[0].items) != 2 || monkeys[0].items[0] != 79 {
+		t.Errorf("Not read items: %v", monkeys[0].items)
+	}
+	if monkeys[0].adjustment != 19 ||
+		monkeys[0].falseMonkey != 3 ||
+		monkeys[0].number != 0 ||
+		monkeys[0].operation != "*" ||
+		monkeys[0].test != 23 ||
+		monkeys[0].trueMonkey != 2 {
+		t.Errorf("Bad monkey: %+v", monkeys[0])
+	}
+}
+
+func Test2022_11_1_Main(t *testing.T) {
+	data := `Monkey 0:
+	Starting items: 79, 98
+	Operation: new = old * 19
+	Test: divisible by 23
+	  If true: throw to monkey 2
+	  If false: throw to monkey 3
+  
+  Monkey 1:
+	Starting items: 54, 65, 75, 74
+	Operation: new = old + 6
+	Test: divisible by 19
+	  If true: throw to monkey 2
+	  If false: throw to monkey 0
+  
+  Monkey 2:
+	Starting items: 79, 60, 97
+	Operation: new = old * old
+	Test: divisible by 13
+	  If true: throw to monkey 1
+	  If false: throw to monkey 3
+  
+  Monkey 3:
+	Starting items: 74
+	Operation: new = old + 3
+	Test: divisible by 17
+	  If true: throw to monkey 0
+	  If false: throw to monkey 1`
+
+	monkeys := buildMonkeys(data)
+
+	if len(monkeys) != 4 {
+		t.Fatalf("Did not read all the monkeys: %v", monkeys)
+	}
+
+	for i := 0; i < 20; i++ {
+		runMonkeys(monkeys)
+	}
+
+	values := getMonkeyTimes(monkeys)
+	if values[0]*values[1] != 10605 {
+		t.Errorf("Bad monkey run: %v -> %v", values[0]*values[1], values)
+	}
+}
+
+func Test2022_11_1_Run(t *testing.T) {
+	data := `Monkey 0:
+	Starting items: 79, 98
+	Operation: new = old * 19
+	Test: divisible by 23
+	  If true: throw to monkey 2
+	  If false: throw to monkey 3
+  
+  Monkey 1:
+	Starting items: 54, 65, 75, 74
+	Operation: new = old + 6
+	Test: divisible by 19
+	  If true: throw to monkey 2
+	  If false: throw to monkey 0
+  
+  Monkey 2:
+	Starting items: 79, 60, 97
+	Operation: new = old * old
+	Test: divisible by 13
+	  If true: throw to monkey 1
+	  If false: throw to monkey 3
+  
+  Monkey 3:
+	Starting items: 74
+	Operation: new = old + 3
+	Test: divisible by 17
+	  If true: throw to monkey 0
+	  If false: throw to monkey 1`
+
+	monkeys := buildMonkeys(data)
+
+	if len(monkeys) != 4 {
+		t.Fatalf("Did not read all the monkeys: %v", monkeys)
+	}
+
+	runMonkeys(monkeys)
+
+	if len(monkeys[0].items) != 4 || monkeys[0].items[0] != 20 {
+		t.Errorf("Bad items: %v, %v, %v, %v", monkeys[0].items, monkeys[1].items, monkeys[2].items, monkeys[3].items)
+	}
+}
