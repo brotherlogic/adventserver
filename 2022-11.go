c635bf3045e6de3134009f08720f5ced1c203044
Simon Tucker <brotherlogic@gmail.com>
2022-12-11 11:32:15 -0800

    Added build monkeys function

--- 2022-11.go
+++ 2022-11.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"strconv"
+	"strings"
+
 	pb "github.com/brotherlogic/adventserver/proto"
 	"golang.org/x/net/context"
 )
 
 type monkey struct {
+	number      int
 	items       []int
 	operation   string
 	adjustment  int
@@ -16,7 +20,48 @@ type monkey struct {
 }
 
 func buildMonkeys(data string) []*monkey {
-	return []*monkey{}
+	var cmonkey *monkey
+	var monkeys []*monkey
+	for _, line := range strings.Split(data, "\n") {
+		nline := strings.TrimSpace(line)
+		if len(nline) > 0 {
+			fields := strings.Fields(nline)
+			switch fields[0] {
+			case "Monkey":
+				num, _ := strconv.ParseInt(fields[1][:len(fields[1])-2], 10, 32)
+				if num > 0 {
+					monkeys = append(monkeys, cmonkey)
+				}
+				cmonkey = &monkey{number: int(num), items: make([]int, 0)}
+
+			case "Starting":
+				tline := strings.Split(nline, ":")
+				nums := strings.Split(tline[1], ",")
+				for _, num := range nums {
+					numv, _ := strconv.ParseInt(num, 10, 32)
+					cmonkey.items = append(cmonkey.items, int(numv))
+				}
+			case "Operation":
+				cmonkey.operation = fields[4]
+				num, _ := strconv.ParseInt(fields[5], 10, 32)
+				cmonkey.adjustment = int(num)
+			case "Test":
+				num, _ := strconv.ParseInt(fields[3], 10, 32)
+				cmonkey.test = int(num)
+			case "If":
+				num, _ := strconv.ParseInt(fields[5], 10, 32)
+				if fields[1] == "true:" {
+					cmonkey.trueMonkey = int(num)
+				} else {
+					cmonkey.falseMonkey = int(num)
+				}
+			}
+		}
+	}
+
+	monkeys = append(monkeys, cmonkey)
+
+	return monkeys
 }
 
 func runMonkeys(monkeys []*monkey) {
