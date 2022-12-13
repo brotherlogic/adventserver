ed3c1c057c04aee6412e9cbb50d2a94189146f97
Simon Tucker <brotherlogic@gmail.com>
2022-12-13 08:14:37 -0800

    Huh

--- 2016-23.go
+++ 2016-23.go
@@ -21,6 +21,7 @@ var (
 type toggler struct {
 	a, b, c, d int
 	program    []string
+	cprogram   []string
 	pointer    int
 }
 
@@ -49,6 +50,7 @@ func runToggleProgram(data string, init int) *toggler {
 	}
 
 	for toggler.pointer < len(toggler.program) {
+		log.Printf("EXEC: %v -> %v %v %v %v", toggler.program[toggler.pointer], toggler.a, toggler.b, toggler.c, toggler.d)
 		nline.Set(float64(toggler.pointer))
 		fields := strings.Fields(toggler.program[toggler.pointer])
 		switch fields[0] {
