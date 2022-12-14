5665bd532b9eb55029a8fe2fabc45556a3817ae4
Simon Tucker <brotherlogic@gmail.com>
2022-12-14 08:22:17 -0800

    Working on 2022, Day 14, Part 1

---
+++ 2022-14_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func Test2022_14_1_Main(t *testing.T) {
+	data := `498,4 -> 498,6 -> 496,6
+	503,4 -> 502,4 -> 502,9 -> 494,9`
+
+	sand := countSand(data)
+
+	if sand != 24 {
+		t.Errorf("Bad sand: %v (24)", sand)
+	}
+}
