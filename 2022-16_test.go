a3a999166c45834fbe2b243807cac51dbaccdedb
Simon Tucker <brotherlogic@gmail.com>
2022-12-17 16:37:29 -0800

    Working on 2022, Day 16, Part 2 (#191)

--- 2022-16_test.go
+++ 2022-16_test.go
@@ -20,3 +20,22 @@ func Test2022_16_1_Main(t *testing.T) {
 		t.Errorf("Bad gas release: %v (1651)", val)
 	}
 }
+
+func Test2022_16_2_Main(t *testing.T) {
+	data := `Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+	Valve BB has flow rate=13; tunnels lead to valves CC, AA
+	Valve CC has flow rate=2; tunnels lead to valves DD, BB
+	Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+	Valve EE has flow rate=3; tunnels lead to valves FF, DD
+	Valve FF has flow rate=0; tunnels lead to valves EE, GG
+	Valve GG has flow rate=0; tunnels lead to valves FF, HH
+	Valve HH has flow rate=22; tunnel leads to valve GG
+	Valve II has flow rate=0; tunnels lead to valves AA, JJ
+	Valve JJ has flow rate=21; tunnel leads to valve II`
+
+	val := releaseGasPair(data, 26)
+
+	if val != 1707 {
+		t.Errorf("Bad gas release: %v (1707)", val)
+	}
+}
