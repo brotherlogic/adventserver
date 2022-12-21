dadc90025d8295df7a9b67f02095c70abb95b8a5
Simon Tucker <brotherlogic@gmail.com>
2022-12-21 14:41:02 -0800

    Try this

--- 2022-20.go
+++ 2022-20.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"strings"
 
 	pb "github.com/brotherlogic/adventserver/proto"
@@ -22,15 +24,27 @@ func unencrpyt(data string) int {
 	}
 
 	for _, val := range arr {
+		log.Printf("MOVE %v", printNumMap(numMap))
 		numMap = moveMap(numMap, val)
 	}
+	log.Printf("DONE %v", printNumMap(numMap))
 
 	narr := make([]int, len(arr))
 	for key, val := range numMap {
 		narr[val] = key
 	}
 
+	log.Printf("FOUND %v", narr)
+
 	return narr[(numMap[0]+1000)%len(narr)] + narr[(numMap[0]+2000)%len(narr)] + narr[(numMap[0]+3000)%len(narr)]
+}
+
+func printNumMap(numMap map[int]int) string {
+	narr := make([]int, len(numMap))
+	for key, val := range numMap {
+		narr[val] = key
+	}
+	return fmt.Sprintf("%v", narr)
 }
 
 func moveNumber(arr []int, num int) []int {
@@ -50,8 +64,30 @@ func moveNumber(arr []int, num int) []int {
 }
 
 func moveMap(numMap map[int]int, num int) map[int]int {
+	if num == 0 {
+		return numMap
+	}
+
 	nIndex := numMap[num]
 	llen := len(numMap)
+
+	newIndex := (nIndex + num) % llen
+	if num < 0 {
+		newIndex = (nIndex + num - 1) % llen
+	}
+	if newIndex < 0 {
+		newIndex = llen + newIndex
+	}
+
+	log.Printf("NEW %v (%v) -> %v", num, nIndex, newIndex)
+	following := 0
+	for key, val := range numMap {
+		if val == newIndex {
+			following = key
+		}
+	}
+
+	log.Printf("INSERT %v-> %v", num, following)
 
 	// Remove the number
 	delete(numMap, num)
@@ -61,16 +97,14 @@ func moveMap(numMap map[int]int, num int) map[int]int {
 		}
 	}
 
-	newIndex := (nIndex + num) % llen
-	if newIndex < 0 {
-		newIndex = llen + newIndex - 1
-	}
+	newNewIndex := numMap[following] + 1
+
 	for key, val := range numMap {
-		if val >= newIndex {
+		if val >= newNewIndex {
 			numMap[key] = val + 1
 		}
 	}
-	numMap[num] = newIndex
+	numMap[num] = newNewIndex
 
 	return numMap
 }
--- 2022-20_test.go
+++ 2022-20_test.go
@@ -5,6 +5,27 @@ import (
 	"testing"
 )
 
+func rotateMatch(a1, a2 []int) bool {
+	zero1, zero2 := 0, 0
+
+	for i := 0; i < len(a1); i++ {
+		if a1[i] == 0 {
+			zero1 = i
+		}
+		if a2[i] == 0 {
+			zero2 = i
+		}
+	}
+
+	for i := 0; i < len(a1); i++ {
+		if a1[(zero1+i)%len(a1)] != a2[(zero2+i)%len(a1)] {
+			return false
+		}
+	}
+
+	return true
+}
+
 func Test2022_20_1_Sup(t *testing.T) {
 
 	cases := []struct {
@@ -14,21 +35,48 @@ func Test2022_20_1_Sup(t *testing.T) {
 	}{
 		{[]int{4, 5, 6, 1, 7, 8, 9}, 1, []int{4, 5, 6, 7, 1, 8, 9}},
 		{[]int{4, -2, 5, 6, 7, 8, 9}, -2, []int{4, 5, 6, 7, 8, -2, 9}},
+		{[]int{1, 2, -3, 3, -2, 0, 4}, 1, []int{2, 1, -3, 3, -2, 0, 4}},
+		{[]int{2, 1, -3, 3, -2, 0, 4}, 2, []int{1, -3, 2, 3, -2, 0, 4}},
+		{[]int{1, 2, -2, -3, 0, 3, 4}, -2, []int{1, 2, -3, 0, 3, 4, -2}},
+		{[]int{1, 2, -3, 0, 3, 4, -2}, 4, []int{1, 2, -3, 4, 0, 3, -2}},
 	}
 
 	for i, cs := range cases {
-		log.Printf("TEST %v", i)
+		log.Printf("RUN %v", cs.in)
 		moved := moveNumber(cs.in, cs.move)
 
-		match := true
-		for i := 0; i < len(moved); i++ {
-			if moved[i] != cs.out[i] {
-				match = false
-			}
-		}
+		match := rotateMatch(moved, cs.out)
 
 		if !match {
-			t.Errorf("Bad match %v;%v -> %v (%v)", cs.in, cs.move, moved, cs.out)
+			t.Errorf("Bad match (%v) %v;%v -> %v (%v)", i, cs.in, cs.move, moved, cs.out)
+		}
+	}
+}
+
+func Test2022_20_1_Steps(t *testing.T) {
+
+	cases := []struct {
+		in   []int
+		move int
+		out  []int
+	}{
+		{[]int{1, 2, -3, 3, -2, 0, 4}, 1, []int{2, 1, -3, 3, -2, 0, 4}},
+		{[]int{2, 1, -3, 3, -2, 0, 4}, 2, []int{1, -3, 2, 3, -2, 0, 4}},
+		{[]int{1, -3, 2, 3, -2, 0, 4}, -3, []int{1, 2, 3, -2, -3, 0, 4}},
+		{[]int{1, 2, 3, -2, -3, 0, 4}, 3, []int{1, 2, -2, -3, 0, 3, 4}},
+		{[]int{1, 2, -2, -3, 0, 3, 4}, -2, []int{1, 2, -3, 0, 3, 4, -2}},
+		{[]int{1, 2, -3, 0, 3, 4, -2}, 0, []int{1, 2, -3, 0, 3, 4, -2}},
+		{[]int{1, 2, -3, 0, 3, 4, -2}, 4, []int{1, 2, -3, 4, 0, 3, -2}},
+	}
+
+	for i, cs := range cases {
+		log.Printf("RUN %v", cs.in)
+		moved := moveNumber(cs.in, cs.move)
+
+		match := rotateMatch(moved, cs.out)
+
+		if !match {
+			t.Errorf("Bad match (%v) %v;%v -> %v (%v)", i, cs.in, cs.move, moved, cs.out)
 		}
 	}
 }
