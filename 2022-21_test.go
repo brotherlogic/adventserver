c243ba0b1efd4db5cecd9932ee714a4c5a295614
Simon Tucker <brotherlogic@gmail.com>
2022-12-22 11:54:52 -0800

    Aha (#204)

--- 2022-21_test.go
+++ 2022-21_test.go
@@ -20,7 +20,37 @@ drzm: hmdt - zczc
 hmdt: 32`
 
 	program := buildProgram(data)
-	if evalProg(program, "root") != 152 {
-		t.Errorf("Bad prog run: %v (152)", evalProg(program, "root"))
+	res, _ := evalProg(program, "root")
+	if res != 152 {
+		t.Errorf("Bad prog run: %v (152)", res)
+	}
+}
+
+func Test2022_21_2_Main(t *testing.T) {
+	data := `root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32`
+
+	program := buildProgram(data)
+	program.progs["humn"].unknown = true
+
+	val, _ := evalProg(program, program.progs["root"].right)
+
+	result := findUnknown(program, program.progs["root"].left, val)
+
+	if result != 301 {
+		t.Errorf("Bad prog run: %v (301)", result)
 	}
 }
