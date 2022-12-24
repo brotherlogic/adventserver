eb7a3e73d50cfee7eeb118debab2e059c4cb1b23
Simon Tucker <brotherlogic@gmail.com>
2022-12-24 07:08:32 -0800

    Fixed to pass

--- 2022-23_test.go
+++ 2022-23_test.go
@@ -16,3 +16,17 @@ func Test2022_23_1_Main(t *testing.T) {
 		t.Errorf("Bad times: %v (110)", result)
 	}
 }
+
+func Test2022_23_1_Sup(t *testing.T) {
+	data := `.....
+..##.
+..#..
+.....
+..##.
+.....`
+
+	result := runElves(data, 3)
+	if result != 30-5 {
+		t.Errorf("Bad times: %v (25)", result)
+	}
+}
