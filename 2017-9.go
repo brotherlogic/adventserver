f2c88c78122537c32d9ebaa552196c6efb28d733
Simon Tucker <brotherlogic@gmail.com>
2022-12-27 08:03:16 -0800

    Fix (#220)

---
+++ 2017-9.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"strings"
+
+	pb "github.com/brotherlogic/adventserver/proto"
+	"golang.org/x/net/context"
+)
+
+func convertStream(line string) int {
+	inGarbage := false
+
+	groups := 0
+	depth := 0
+	pointer := 0
+	for pointer < len(line) {
+		switch line[pointer] {
+		case '!':
+			pointer++
+		case '{':
+			if !inGarbage {
+				depth++
+			}
+		case '}':
+			if !inGarbage {
+				groups += depth
+				depth--
+			}
+		case '<':
+			inGarbage = true
+		case '>':
+			inGarbage = false
+		}
+
+		pointer++
+	}
+
+	return groups
+}
+
+func (s *Server) Solve2017day9part1(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2017-9.txt")
+	if err != nil {
+		return nil, err
+	}
+	sumv := int32(0)
+	for _, line := range strings.Split(strings.TrimSpace(data), "\n") {
+		sumv += int32(convertStream(strings.TrimSpace(line)))
+	}
+	return &pb.SolveResponse{Answer: sumv}, nil
+}
