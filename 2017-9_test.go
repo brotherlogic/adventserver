fe71b23d58ff241fc9677555c06f93da579dd485
Simon Tucker <brotherlogic@gmail.com>
2022-12-27 08:15:09 -0800

    Fixed part 2 (#221)

--- 2017-9_test.go
+++ 2017-9_test.go
@@ -24,3 +24,25 @@ func Test2017_9_1_Main(t *testing.T) {
 		}
 	}
 }
+
+func Test2017_9_2_Main(t *testing.T) {
+	tests := []struct {
+		in  string
+		out int
+	}{
+		{"<>", 0},
+		{"<random characters>", 17},
+		{"<<<<>", 3},
+		{"<{!>}>", 2},
+		{"<!!>", 0},
+		{"<!!!>>", 0},
+		{"<{o\"i!a,<{i<a>", 10},
+	}
+
+	for _, test := range tests {
+		result := countGarbage(test.in)
+		if result != test.out {
+			t.Errorf("Bad test %v -> %v (%v)", test.in, result, test.out)
+		}
+	}
+}
