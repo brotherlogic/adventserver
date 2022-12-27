d58013ade9c286dd943313b43636ae399b96d13c
Simon Tucker <brotherlogic@gmail.com>
2022-12-27 08:11:12 -0800

    Fixed part 2

--- 2017-9.go
+++ 2017-9.go
@@ -38,6 +38,43 @@ func convertStream(line string) int {
 	return groups
 }
 
+func countGarbage(line string) int {
+	inGarbage := false
+
+	groups := 0
+	depth := 0
+	pointer := 0
+	garbage := 0
+	for pointer < len(line) {
+		if inGarbage {
+			garbage++
+		}
+		switch line[pointer] {
+		case '!':
+			pointer++
+			garbage--
+		case '{':
+			if !inGarbage {
+				depth++
+			}
+		case '}':
+			if !inGarbage {
+				groups += depth
+				depth--
+			}
+		case '<':
+			inGarbage = true
+		case '>':
+			inGarbage = false
+			garbage--
+		}
+
+		pointer++
+	}
+
+	return garbage
+}
+
 func (s *Server) Solve2017day9part1(ctx context.Context) (*pb.SolveResponse, error) {
 	data, err := s.loadFile(ctx, "/media/scratch/advent/2017-9.txt")
 	if err != nil {
@@ -46,6 +83,18 @@ func (s *Server) Solve2017day9part1(ctx context.Context) (*pb.SolveResponse, err
 	sumv := int32(0)
 	for _, line := range strings.Split(strings.TrimSpace(data), "\n") {
 		sumv += int32(convertStream(strings.TrimSpace(line)))
+	}
+	return &pb.SolveResponse{Answer: sumv}, nil
+}
+
+func (s *Server) Solve2017day9part2(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2017-9.txt")
+	if err != nil {
+		return nil, err
+	}
+	sumv := int32(0)
+	for _, line := range strings.Split(strings.TrimSpace(data), "\n") {
+		sumv += int32(countGarbage(strings.TrimSpace(line)))
 	}
 	return &pb.SolveResponse{Answer: sumv}, nil
 }
