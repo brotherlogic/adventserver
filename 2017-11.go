5f4b1fa8d090d983a9fc10ae451c2feb5a8cd707
Simon Tucker <brotherlogic@gmail.com>
2023-01-12 08:57:43 -0800

    Also track best

--- 2017-11.go
+++ 2017-11.go
@@ -9,9 +9,28 @@ import (
 	pb "github.com/brotherlogic/adventserver/proto"
 )
 
-func computeSteps(data string) int {
+func getDist(x, y int) int {
+	// Move to a diag
+	if x == 0 && y == 0 {
+		return 0
+	}
+
+	// We are below a diagnol
+	if abs(y) > abs(x) {
+		return (abs(y)-abs(x))/2 + abs(x)
+	}
+
+	if abs(y) == abs(x) {
+		return abs(x)
+	}
+
+	return 0
+}
+
+func computeSteps(data string) (int, int) {
 	x, y := 0, 0
 
+	best := 0
 	for _, elem := range strings.Split(data, ",") {
 		switch elem {
 		case "n":
@@ -33,23 +52,13 @@ func computeSteps(data string) int {
 		default:
 			log.Fatalf("Unknown direction: %v", elem)
 		}
+		curr := getDist(x, y)
+		if curr > best {
+			best = curr
+		}
 	}
 
-	// Move to a diag
-	if x == 0 && y == 0 {
-		return 0
-	}
-
-	// We are below a diagnol
-	if abs(y) > abs(x) {
-		return (abs(y)-abs(x))/2 + abs(x)
-	}
-
-	if abs(y) == abs(x) {
-		return abs(x)
-	}
-
-	return 0
+	return getDist(x, y), best
 }
 
 func (s *Server) Solve2017day11part1(ctx context.Context) (*pb.SolveResponse, error) {
@@ -58,5 +67,16 @@ func (s *Server) Solve2017day11part1(ctx context.Context) (*pb.SolveResponse, er
 		return nil, err
 	}
 
-	return &pb.SolveResponse{Answer: int32(computeSteps(strings.TrimSpace(data)))}, nil
+	curr, _ := computeSteps(strings.TrimSpace(data))
+	return &pb.SolveResponse{Answer: int32(curr)}, nil
+}
+
+func (s *Server) Solve2017day11part2(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2017-11.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	_, curr := computeSteps(strings.TrimSpace(data))
+	return &pb.SolveResponse{Answer: int32(curr)}, nil
 }
