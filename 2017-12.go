a64c5f3cea5a0b1bb40ce79951ad3e9ee1b399a7
Simon Tucker <brotherlogic@gmail.com>
2023-01-13 14:20:46 -0800

    Working on 2017, Day 12, Part 2

--- 2017-12.go
+++ 2017-12.go
@@ -16,6 +16,47 @@ var (
 	})
 )
 
+func countGroups(data string) int {
+
+	connections := make(map[int][]int)
+
+	for _, line := range strings.Split(strings.TrimSpace(data), "\n") {
+		bits := strings.Split(line, "<->")
+		num1 := getInt32(bits[0])
+		if _, ok := connections[num1]; !ok {
+			connections[num1] = make([]int, 0)
+		}
+
+		for _, bit := range strings.Split(bits[1], ",") {
+			num2 := getInt32(strings.TrimSpace(bit))
+			if _, ok := connections[num2]; !ok {
+				connections[num2] = make([]int, 0)
+			}
+
+			connections[num1] = append(connections[num1], num2)
+			connections[num2] = append(connections[num2], num1)
+		}
+	}
+
+	seen := make(map[int]bool)
+	queue := []int{0}
+
+	for len(queue) > 0 {
+		zlen.Set(float64(len(queue)))
+		head := queue[0]
+		queue = queue[1:]
+
+		for _, next := range connections[head] {
+			if !seen[next] {
+				queue = append(queue, next)
+				seen[next] = true
+			}
+		}
+	}
+
+	return len(seen)
+}
+
 func countZeros(data string) int {
 
 	connections := make(map[int][]int)
@@ -64,4 +105,13 @@ func (s *Server) Solve2017day12part1(ctx context.Context) (*pb.SolveResponse, er
 	}
 
 	return &pb.SolveResponse{Answer: int32(countZeros(data))}, nil
+}
+
+func (s *Server) Solve2017day12part2(ctx context.Context) (*pb.SolveResponse, error) {
+	data, err := s.loadFile(ctx, "/media/scratch/advent/2017-12.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.SolveResponse{Answer: int32(countGroups(data))}, nil
 }
